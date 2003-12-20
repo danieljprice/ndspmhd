70a4f20fab03f9781c17324dca89567235bdae32
dprice <dprice>
2003-12-20 00:24:49 +0000

    partial neighbour finding in multi-D

--- src/iterate_density.f90
+++ src/iterate_density.f90
@@ -13,122 +13,102 @@
 !! For details see Price and Monaghan 2003b
 !!------------------------------------------------------------------------
 
-SUBROUTINE iterate_density
- USE dimen_mhd
- USE debug
- USE loguns
-
- USE bound
- USE hterms
- USE options
- USE part
- USE part_in	! for rhoin on fixed particles
- USE setup_params
+subroutine iterate_density
+  use dimen_mhd
+  use debug
+  use loguns
+  
+  use bound
+  use hterms
+  use options
+  use part
+  use part_in	! for rhoin on fixed particles
+  use setup_params
 !
 !--define local variables
 !
- IMPLICIT NONE
- INTEGER :: i,j,itsdensity,itsdensitymax
- INTEGER :: ncalc,ncalcprev,isize
- INTEGER, DIMENSION(:), ALLOCATABLE :: redolist, redolistprev
- REAL :: tol,hnew
- REAL, DIMENSION(:), ALLOCATABLE :: rho_old,gradh_old
- LOGICAL :: converged,redolink
+  implicit none
+  integer :: i,j,itsdensity,itsdensitymax
+  integer :: ncalc,ncalcprev,isize
+  integer, dimension(:), allocatable :: redolist, redolistprev
+  real :: tol,hnew
+  real, dimension(:), allocatable :: rho_old,gradh_old
+  logical :: converged,redolink
 !
 !--allow for tracing flow
 !      
- IF (trace) WRITE(iprint,*) ' Entering subroutine iterate_density'  
+  if (trace) write(iprint,*) ' Entering subroutine iterate_density' 
 !
 !--allocate memory for local array copies
 !
- isize = SIZE(rho)
- ALLOCATE( rho_old(isize), gradh_old(isize) )
- ALLOCATE( redolist(ntotal), redolistprev(ntotal) )
+  isize = size(rho)
+  allocate( rho_old(isize), gradh_old(isize) )
+  allocate( redolist(ntotal), redolistprev(ntotal) )
 !
 !--set maximum number of iterations to perform
 ! 
- IF ((ikernav.EQ.3).AND.(ihvar.NE.0)) THEN
-    itsdensitymax = 20	! perform 1 fixed point iteration
- ELSE
-    itsdensitymax = 0	! no iterations
- ENDIF
+  if ((ikernav.eq.3).and.(ihvar.ne.0)) then
+     itsdensitymax = 20	! perform 1 fixed point iteration
+  else
+     itsdensitymax = 0	! no iterations
+  endif
 !
 !--Loop to find rho and h self-consistently (if using Springel/Hernquist)
 !
- itsdensity = 0
- tol = 1.e-2
- ncalc = npart	! number of particles to calculate density on
- redolink = .false.
- ncalcprev = 0
- IF (ncalc.EQ.npart) THEN
-    DO j=1,npart     
-       redolist(j) = j
-    ENDDO
- ENDIF
- 
- iterate: DO WHILE ((ncalc.GT.0).AND.(itsdensity.LE.itsdensitymax)    &
-                    .AND. ncalc.NE.ncalcprev)
-
-    itsdensity = itsdensity + 1
-    IF (isize.NE.SIZE(rho)) THEN
-       DEALLOCATE(rho_old,gradh_old)
-       isize = SIZE(rho)
-       ALLOCATE(rho_old(isize),gradh_old(isize))
-    ENDIF
-    rho_old = rho
-    gradh_old = gradh
-    IF (redolink) THEN
-       IF (ANY(ibound.GT.1)) CALL set_ghost_particles
-       IF (idebug(1:3).EQ.'den') WRITE(iprint,*) 'relinking...'
-       CALL link
-    ENDIF
-    
-    IF (ncalc.EQ.npart) THEN	! calculate density on all particles
-       CALL density		! do symmetrically
-    ELSE	! calculate density on partial list of particles              
-!       CALL density             !_partial(ncalc,redolist)
-!       DO j=1,ncalc
-!          i = redolist(j)
-!          PRINT*,' normal  rho(',i,' =',rho(i),gradh(i)
-!       ENDDO
-!       gradh = gradh_old      
-!       print*,'rho(717) 1 = ',rho(717)
-!       rho = rho_old
-!       print*,'rho(717) 2 = ',rho(717)
-!       ncalc = npart
-       IF (ncalc.EQ.npart) THEN
-          DO j=1,npart     
-             redolist(j) = j
-          ENDDO
-       ENDIF
-       CALL density_partial(ncalc,redolist)
-!       DO j=1,ncalc
-!          i = redolist(j)
-!          PRINT*,' partial rho(',i,' =',rho(i),gradh(i)
-!       ENDDO             
-!       READ*
-    ENDIF
-
-    ncalcprev = ncalc
-    redolistprev(1:ncalcprev) = redolist(1:ncalcprev)
-    ncalc = 0
-    redolink = .false.
-
-    IF (ihvar.NE.0) THEN
-       DO j=1,ncalcprev
-          i = redolistprev(j)
-          IF (itype(i).NE.1) THEN
-             IF (rho(i).LE.1.e-3) THEN
-	        IF (rho(i).LE.0.) THEN
-	           WRITE(iprint,*) 'rho: rho -ve, dying rho(',i,') = ',rho(i)
-	           CALL quit
-	        ELSE
-	           WRITE(iprint,*) 'Warning : rho < 1e-5 '
-	        ENDIF
-             ENDIF
-
-             gradh(i) = gradh(i)/rho_old(i)    ! now that rho is known
-             IF (abs(1.-gradh(i)).LT.1.e-5) PRINT*,'eek'
+  itsdensity = 0
+  tol = 1.e-2
+  ncalc = npart	! number of particles to calculate density on
+  redolink = .false.
+  ncalcprev = 0
+  if (ncalc.eq.npart) then
+     do j=1,npart     
+        redolist(j) = j
+     enddo
+  endif
+  
+  iterate: do while ((ncalc.gt.0).and.(itsdensity.le.itsdensitymax)    &
+       .and. ncalc.ne.ncalcprev)
+     
+     itsdensity = itsdensity + 1
+     if (isize.ne.size(rho)) then
+        deallocate(rho_old,gradh_old)
+        isize = size(rho)
+        allocate(rho_old(isize),gradh_old(isize))
+     endif
+     rho_old = rho
+     gradh_old = gradh
+     if (redolink) then
+        if (any(ibound.gt.1)) call set_ghost_particles
+        if (idebug(1:3).eq.'den') write(iprint,*) 'relinking...'
+        call link
+     endif
+     
+     if (ncalc.eq.npart) then	! calculate density on all particles
+        call density		! do this symmetrically
+     else	                ! calculate density on partial list of particles              
+        call density_partial(ncalc,redolist)
+     endif
+     
+     ncalcprev = ncalc
+     redolistprev(1:ncalcprev) = redolist(1:ncalcprev)
+     ncalc = 0
+     redolink = .false.
+     
+     if (ihvar.ne.0) then
+        do j=1,ncalcprev
+           i = redolistprev(j)
+           if (itype(i).ne.1) then
+              if (rho(i).le.1.e-3) then
+                 if (rho(i).le.0.) then
+                    write(iprint,*) 'rho: rho -ve, dying rho(',i,') = ',rho(i)
+                    call quit
+                 else
+                    write(iprint,*) 'Warning : rho < 1e-5 '
+                 endif
+              endif
+              
+              gradh(i) = gradh(i)/rho_old(i)    ! now that rho is known
+              if (abs(1.-gradh(i)).lt.1.e-5) print*,'eek'
 !
 !--perform Newton-Raphson iteration on rho
 !      
@@ -136,54 +116,51 @@ SUBROUTINE iterate_density
 !
 !--work out new smoothing length h
 !
-             hnew = hfact*(pmass(i)/rho(i))**hpower	! ie h proportional to 1/rho^dimen
+              hnew = hfact*(pmass(i)/rho(i))**hpower	! ie h proportional to 1/rho^dimen
 !
 !--if this particle is not converged, add to list of particles to recalculate
 !
-!            PRINT*,'hnew - hh(i) = ',abs(hnew-hh(i))/hh(i)
-
-             converged = abs((hnew-hh(i))/hh(i)) < tol	  
-	     IF (.not.converged) THEN
-	        ncalc = ncalc + 1
-	        redolist(ncalc) = i
-!	        PRINT*,'not converged',i,abs(hnew-hh(i))/hh(i),rho(i),	&
-!     	        ncalc,redolist(ncalc)
+!             PRINT*,'hnew - hh(i) = ',abs(hnew-hh(i))/hh(i)
+              
+              converged = abs((hnew-hh(i))/hh(i)) < tol	  
+              if (.not.converged) then
+                 ncalc = ncalc + 1
+                 redolist(ncalc) = i
+!	         PRINT*,'not converged',i,abs(hnew-hh(i))/hh(i),rho(i),	&
+!     	         ncalc,redolist(ncalc)
 !
 !--update smoothing length only if taking another iteration
 !
- 	        IF (itsdensity.LT.itsdensitymax) hh(i) = hnew
-	        IF (hnew.GT.hhmax) THEN
-		   redolink = .true.
-	        ENDIF	
-	     ENDIF
-          ENDIF	! itype .NE. 1
-       ENDDO
-
-       IF ((idebug(1:3).EQ.'den').AND.(ncalc.GT.0)) THEN
-	  WRITE(iprint,*) ' density, iteration ',itsdensity,' ncalc = ',ncalc,':',redolist(1:ncalc)
-!         DO i=1,ncalc
-!	     PRINT*,' i = ',redolist(i)
-!	  ENDDO
-       ENDIF  
-
-    ENDIF
-
-    IF (ANY(ibound.GT.1)) THEN
-       DO i=npart+1,ntotal		! update ghosts
-          j = ireal(i)
-          rho(i) = rho(j)
-          hh(i) = hh(j)
-          gradh(i) = gradh(j)       
-       ENDDO
-    ELSEIF (ANY(ibound.EQ.1)) THEN		! update fixed particles
-       WHERE (itype(:).EQ.1)
-          rho(:) = rhoin(:)
-       END WHERE
-    ENDIF
-            
- ENDDO iterate
-
- IF (itsdensity.GT.1) WRITE(iprint,*) ' Finished density, iterations = ',itsdensity
- 
- RETURN
-END SUBROUTINE iterate_density
+                 if (itsdensity.lt.itsdensitymax) hh(i) = hnew
+                 if (hnew.gt.hhmax) then
+                    redolink = .true.
+                 endif
+              endif
+           endif	! itype .NE. 1
+        enddo
+        
+        if ((idebug(1:3).eq.'den').and.(ncalc.gt.0)) then
+           write(iprint,*) ' density, iteration ',itsdensity,' ncalc = ',ncalc,':',redolist(1:ncalc)
+        endif
+        
+     endif
+     
+     if (any(ibound.gt.1)) then
+        do i=npart+1,ntotal		! update ghosts
+           j = ireal(i)
+           rho(i) = rho(j)
+           hh(i) = hh(j)
+           gradh(i) = gradh(j)       
+        enddo
+     elseif (any(ibound.eq.1)) then		! update fixed particles
+        where (itype(:).eq.1)
+           rho(:) = rhoin(:)
+        end where
+     endif
+     
+  enddo iterate
+  
+  if (itsdensity.gt.1) write(iprint,*) ' Finished density, iterations = ',itsdensity
+  
+  return
+end subroutine iterate_density
