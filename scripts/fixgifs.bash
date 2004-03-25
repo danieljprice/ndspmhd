79332c0006ff867b8e33a3358b80b8efd0f4d508
dprice <dprice>
2004-03-25 13:13:02 +0000

    up to 10000

--- scripts/fixgifs.bash
+++ scripts/fixgifs.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # @(#) renames pgplot filenames so they are listed in the correct order
 # @(#) (replaces _ with _0 in single then double digit filenames)
-# @(#) NB: only works for < 1000 files
+# @(#) NB: only works for < 10000 files
 
 for x in *_?;
 do if test $x != '*_?'; then echo $x ${x/_/_0}; mv $x ${x/_/_0}; fi;
@@ -11,6 +11,6 @@ for x in *_??;
 do if test $x != '*_??'; then echo $x ${x/_/_0}; mv $x ${x/_/_0}; fi;
 done;
 
-##for x in *_???;
-##do if test $x != '_???'; then echo $x ${x/_/_0}; mv $x ${x/_/_0}; fi;
-##done;
+for x in *_???;
+do if test $x != '_???'; then echo $x ${x/_/_0}; mv $x ${x/_/_0}; fi;
+done;
