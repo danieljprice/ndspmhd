bcff49d9500409fa844cf6c30657ad7228088698
dprice <dprice>
2004-04-28 13:35:43 +0000

    force etc output for debugging

--- src/outputND_mhd.f90
+++ src/outputND_mhd.f90
@@ -38,19 +38,19 @@ SUBROUTINE output(t,nstep)
 !
 !--write timestep to log file
 !      
- WRITE (iprint,99002) t,nstep,npart,ntotal-npart
-99002 FORMAT('| time = ',f7.3,' | timesteps = ',i8,' | npart = ',i7,	&
+ WRITE (iprint,10) t,nstep,npart,ntotal-npart
+10 FORMAT('| time = ',f7.3,' | timesteps = ',i8,' | npart = ',i7,	&
              ' | nghost = ',i5,' |')
 !
 !--write header line to this data block in data file
 !
  IF (imhd.NE.0) THEN
-    ndata = ndim + 8 + 2*ndimB + ndimV	! number of columns apart from co-ords
+    ndata = ndim + 8 + 2*ndimB + ndimV	+2*ndimV ! number of columns apart from co-ords
  ELSE
     ndata = ndim + 6 + ndimV
  ENDIF
- WRITE(idatfile,10) t,npart,nprint,gamma,hfact,ndim,ndimV,ndata
-10 FORMAT(e12.5,1x,i8,1x,i8,1x,f14.12,1x,f6.2,1x,i1,1x,i1,1x,i3)
+ WRITE(idatfile,20) t,npart,nprint,gamma,hfact,ndim,ndimV,ndata
+20 FORMAT(e12.5,1x,i8,1x,i8,1x,f14.12,1x,f6.2,1x,i1,1x,i1,1x,i3)
 
 !
 !--write data for this time to data file
@@ -65,7 +65,7 @@ SUBROUTINE output(t,nstep)
     IF (imhd.NE.0) THEN	! MHD
 
        WRITE(idatfile,30) x(:,i),vel(:,i),dens(i),pr(i),uu(i),hh(i),   &
-        pmass(i),alpha(i),Bfield(:,i),divB(i),curlB(:,i),psi(i)
+        pmass(i),alpha(i),Bfield(:,i),divB(i),curlB(:,i),psi(i),avsource(:,i),force(:,i)
 
     ELSE   ! non-MHD
 
@@ -83,7 +83,7 @@ SUBROUTINE output(t,nstep)
  ierr(3) = flush(iprint)
  IF ( ANY(ierr.NE.0) ) WRITE(*,*) 'Error flushing files, see Makefile ',ierr 
   			  
-30    FORMAT (24(1pe14.6,1x),:)	! make sure the format statement has >/=	
+30    FORMAT (28(1pe14.6,1x),:)	! make sure the format statement has >/=	
 				! max number of columns in the write statement
 
  RETURN
