bd686c0c03c5543e9848a2ad734c1aa2003e33a7
dprice <dprice>
2004-05-07 11:27:52 +0000

    removed fit_quadratic

--- src/utils.f90
+++ src/utils.f90
@@ -25,23 +25,3 @@ subroutine minmaxave(x,xmin,xmax,xav,npts)
 
   return
 end subroutine minmaxave
-
-!-------------------------------------------------------------------
-! small utility to fit a quadratic function 
-! given two points (x0,y0), (x1,y1) and a gradient dydx(x1)
-! returns the coefficients of y = ax^2 + bx + c
-!-------------------------------------------------------------------
-subroutine fit_quadratic(x0,x1,y0,y1,dydx1,aa,bb,cc)
- implicit none
- real, intent(in) :: x0,x1,y0,y1,dydx1
- real, intent(out) :: aa,bb,cc
-
- aa = ((y1 - y0) - dydx1*(x1-x0))/(x1**2 - x0**2 - 2.*x1*(x1-x0))
- bb = dydx1 - 2.*aa*x1
- cc = y0 - aa*x0**2 - bb*x0
-
-!! print 10,aa,bb,cc
-10 format(/,'quadratic: y = ',f9.3,' x^2 + ',f9.3,' x + ',f9.3)
-
- return
-end subroutine fit_quadratic
