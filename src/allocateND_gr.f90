bf8502a57937b2b9d3900cb1eac56d5956f48a6b
dprice <dprice>
2004-05-07 11:30:28 +0000

    versions for gr code

---
+++ src/allocateND_gr.f90
@@ -0,0 +1,300 @@
+!!----------------------------------------------------------------------
+!! This subroutine allocates/reallocates memory for all arrays
+!! given the new number of particles (should be initially called from setup).
+!!----------------------------------------------------------------------
+SUBROUTINE alloc(newsizein)
+!
+!--include relevant global variables
+!
+ USE dimen_mhd
+ USE debug
+ USE loguns
+ 
+ USE bound
+ USE derivB
+ USE eos
+ USE fmagarray
+ USE gravity
+ USE hterms
+ USE linklist
+ USE options
+ USE part
+ USE part_in
+ USE rates
+ USE xsph
+!
+!--define local variables
+!
+ IMPLICIT NONE
+ INTEGER, INTENT(IN) :: newsizein
+ INTEGER :: newsize,ioldsize,idumsize
+ REAL, DIMENSION(newsizein) :: dumpmass,dumrhoin,dumenin,dumalphain
+ REAL, DIMENSION(newsizein) :: dumrho,dumdrhodt,dumuu,dumdudt,dumen,dumdendt
+ REAL, DIMENSION(newsizein) :: dumalpha,dumdaldt,dumhh,dumgradh,dumpr,dumspsound
+ REAL, DIMENSION(newsizein) :: dumdivB,dumhhin,dumdhdt
+ REAL, DIMENSION(ndim,newsizein) :: dumxin,dumx,dumfgrav
+ REAL, DIMENSION(ndimV,newsizein) :: dumpmomin,dumvel,dumBconsin
+ REAL, DIMENSION(ndimV,newsizein) :: dumforce,dumBcons,dumdBconsdt,dumfmag
+ REAL, DIMENSION(ndimV,newsizein) :: dumBfield,dumcurlB,dumxsphterm
+!--gr terms
+ REAL, DIMENSION(newsizein) :: dumsqrtg,dumdens
+ REAL, DIMENSION(ndimV,newsizein) :: dumsourceterms,dumpmom
+ INTEGER, DIMENSION(newsizein) :: idumireal,idumitype
+ LOGICAL :: reallocate
+!
+!--set size of neighbour list
+!
+ nlistdim = newsizein/2	! ie. up to half of particles can be neighbours
+!
+!--work out whether reallocating memory and what the current array size is
+! 
+ reallocate = ALLOCATED(x)
+ IF (allocated(rho)) THEN
+    ioldsize = SIZE(rho)  ! current array size
+ ELSE
+    ioldsize = 0
+ ENDIF
+!
+!--check for errors
+! 
+ IF (newsizein.EQ.0) THEN
+    WRITE(iprint,*) 'Error: size=0 in call to allocate'
+    CALL quit
+ ENDIF
+!
+!--set new size (add 10% so don't have to do this too often)
+!
+ IF (ANY(ibound.GE.1) .AND. newsizein.GE.ioldsize) THEN
+    newsize = INT(1.1*newsizein)
+ ELSE
+    newsize = newsizein   
+ ENDIF
+
+ IF (reallocate) THEN
+    WRITE(iprint,*) 'Reallocating memory for all arrays, old, new =',SIZE(rho),newsize
+ ELSEIF (trace) THEN
+    WRITE(iprint,*) 'Allocating memory for all arrays',newsize
+ ENDIF
+ 
+ IF (reallocate) THEN
+    idumsize = MIN(newsize,ioldsize)  ! dummy array size is minimum of old/new
+!-----------------------------------------------------------------------------
+!  if reallocating, copy everything into dummy arrays using the smallest size
+!-----------------------------------------------------------------------------
+    dumpmass(1:idumsize) = pmass(1:idumsize)
+    dumrhoin(1:idumsize) = rhoin(1:idumsize)
+    dumhhin(1:idumsize) = hhin(1:idumsize)
+    dumenin(1:idumsize) = enin(1:idumsize)
+    dumalphain(1:idumsize) = alphain(1:idumsize)
+    
+    dumxin(:,1:idumsize) = xin(:,1:idumsize)
+    dumpmomin(:,1:idumsize) = pmomin(:,1:idumsize)
+    dumBconsin(:,1:idumsize) = Bconsin(:,1:idumsize)
+    
+    dumx(:,1:idumsize) = x(:,1:idumsize)
+    dumvel(:,1:idumsize) = vel(:,1:idumsize)
+    dumforce(:,1:idumsize) = force(:,1:idumsize)
+    dumBcons(:,1:idumsize) = Bcons(:,1:idumsize)
+    dumBfield(:,1:idumsize) = Bfield(:,1:idumsize)
+    dumdBconsdt(:,1:idumsize) = dBconsdt(:,1:idumsize)
+    dumfmag(:,1:idumsize) = fmag(:,1:idumsize)
+    dumcurlB(:,1:idumsize) = curlB(:,1:idumsize)
+    dumxsphterm(:,1:idumsize) = xsphterm(:,1:idumsize)
+        
+    dumrho(1:idumsize) = rho(1:idumsize)
+    dumdrhodt(1:idumsize) = drhodt(1:idumsize)
+    dumuu(1:idumsize) = uu(1:idumsize)
+    dumdudt(1:idumsize) = dudt(1:idumsize)
+    dumen(1:idumsize) = en(1:idumsize)
+    dumdendt(1:idumsize) = dendt(1:idumsize)
+    dumalpha(1:idumsize) = alpha(1:idumsize)
+    dumdaldt(1:idumsize) = daldt(1:idumsize)
+    dumhh(1:idumsize) = hh(1:idumsize)
+    dumdhdt(1:idumsize) = dhdt(1:idumsize)
+    dumgradh(1:idumsize) = gradh(1:idumsize)
+    dumpr(1:idumsize) = pr(1:idumsize)
+    dumspsound(1:idumsize) = spsound(1:idumsize)
+!    dumll(1:idumsize) = ll(1:idumsize)
+    dumdivB(1:idumsize) = divB(1:idumsize)
+    idumireal(1:idumsize) = ireal(1:idumsize)
+    idumitype(1:idumsize) = itype(1:idumsize)
+    IF (ALLOCATED(fgrav)) dumfgrav(:,1:idumsize) = fgrav(:,1:idumsize)
+    dumsqrtg(1:idumsize) = sqrtg(1:idumsize)
+    dumdens(1:idumsize) = dens(1:idumsize)    
+    IF (ALLOCATED(sourceterms)) dumsourceterms(:,1:idumsize) = sourceterms(:,1:idumsize)
+    dumpmom(:,1:idumsize) = pmom(:,1:idumsize)
+ 
+!-----------------------------------------------------------------------------
+!  deallocate the arrays
+!-----------------------------------------------------------------------------
+!
+!--initial particle properties
+!
+    DEALLOCATE (pmass,xin,rhoin,hhin,enin,alphain)
+    DEALLOCATE (pmomin)
+    IF (ALLOCATED(Bconsin)) DEALLOCATE (Bconsin)
+!
+!--particle properties and derivatives
+!
+    DEALLOCATE(x,vel,force,rho,drhodt,uu,dudt,en,dendt)
+    DEALLOCATE(alpha,daldt,hh,dhdt,gradh,pr)
+    IF (ALLOCATED(Bfield)) DEALLOCATE(Bfield)
+    IF (ALLOCATED(Bcons)) DEALLOCATE(Bcons)
+    IF (ALLOCATED(dBconsdt)) DEALLOCATE(dBconsdt)
+!
+!--equation of state
+!
+    DEALLOCATE(spsound)
+!
+!--linklist and boundaries - note ifirstincell is reallocated in link
+!  (depends on max # of link list cells)
+!
+    DEALLOCATE(ll,ireal,ifirstincell,iamincell)
+!
+!--itype is particle type (normal, fixed particle etc)
+!
+    IF (ALLOCATED(itype)) DEALLOCATE(itype)
+!
+!--MHD quantities and derivatives
+!
+    IF (ALLOCATED(fmag)) DEALLOCATE(fmag)
+    IF (ALLOCATED(divB)) DEALLOCATE(divB)
+    IF (ALLOCATED(curlB)) DEALLOCATE(curlB)
+!
+!--XSPH
+!
+    IF (ALLOCATED(xsphterm)) DEALLOCATE(xsphterm)
+!
+!--gravity
+!
+    IF (ALLOCATED(fgrav)) DEALLOCATE(fgrav)
+!
+!--GR
+!
+    IF (ALLOCATED(sqrtg)) DEALLOCATE(sqrtg)
+    IF (ALLOCATED(sourceterms)) DEALLOCATE(sourceterms)
+    IF (ALLOCATED(pmom)) DEALLOCATE(pmom)
+    IF (ALLOCATED(dens)) DEALLOCATE(dens)
+ ENDIF
+
+!-----------------------------------------------------------------------------
+!  allocate all arrays (for both first time and reallocation)
+!-----------------------------------------------------------------------------
+
+!
+!--initial particle properties
+!
+    ALLOCATE (pmass(newsize))
+    ALLOCATE (xin(ndim,newsize))
+    ALLOCATE (rhoin(newsize),hhin(newsize))
+    ALLOCATE (enin(newsize),alphain(newsize))
+    ALLOCATE (pmomin(ndimV,newsize))
+    ALLOCATE (Bconsin(ndimB,newsize))
+!
+!--particle properties and derivatives
+!
+    ALLOCATE(x(ndim,newsize))
+    ALLOCATE(vel(ndimV,newsize),force(ndimV,newsize))
+    ALLOCATE(rho(newsize),drhodt(newsize))
+    ALLOCATE(uu(newsize),dudt(newsize),en(newsize),dendt(newsize))
+    ALLOCATE(alpha(newsize),daldt(newsize))
+    ALLOCATE(hh(newsize),dhdt(newsize),gradh(newsize),pr(newsize))
+    ALLOCATE(Bcons(ndimB,newsize))
+    ALLOCATE(Bfield(ndimB,newsize),dBconsdt(ndimB,newsize))	! mag field
+!
+!--equation of state
+!
+    ALLOCATE(spsound(newsize))
+!
+!--linklist and boundaries - note ifirstincell is reallocated in link
+!  (depends on max # of link list cells)
+!
+    ALLOCATE(ll(newsize),iamincell(newsize),ifirstincell(0),ireal(newsize))
+!
+!--particle type
+!
+    ALLOCATE(itype(newsize))
+!
+!--MHD quantities and derivatives
+!
+    ALLOCATE(fmag(ndimB,newsize),divB(newsize),curlB(ndimB,newsize))
+!
+!--XSPH
+!
+    ALLOCATE(xsphterm(ndimV,newsize))
+!
+!--gravity
+!   
+    IF (igravity.NE.0) THEN
+       ALLOCATE(fgrav(ndim,newsize))
+    ENDIF
+!
+!--GR
+!
+   ALLOCATE(sqrtg(newsize))
+   ALLOCATE(dens(newsize))
+   ALLOCATE(pmom(ndimV,newsize))
+   IF (igeom.NE.0) THEN
+      ALLOCATE(sourceterms(ndimV,newsize))
+   ENDIF
+   
+ IF (reallocate) THEN
+!-----------------------------------------------------------------------------
+!  copy properties back from old arrays to new arrays
+!-----------------------------------------------------------------------------
+    pmass(1:idumsize) = dumpmass(1:idumsize)
+    rhoin(1:idumsize) = dumrhoin(1:idumsize)
+    hhin(1:idumsize) = dumhhin(1:idumsize)
+    enin(1:idumsize) = dumenin(1:idumsize)
+    alphain(1:idumsize) = dumalphain(1:idumsize)
+    
+    xin(:,1:idumsize) = dumxin(:,1:idumsize)
+    pmomin(:,1:idumsize) = dumpmomin(:,1:idumsize)
+    Bconsin(:,1:idumsize) = dumBconsin(:,1:idumsize)
+    
+    x(:,1:idumsize) = dumx(:,1:idumsize)
+    vel(:,1:idumsize) = dumvel(:,1:idumsize)
+    force(:,1:idumsize) = dumforce(:,1:idumsize)
+    Bfield(:,1:idumsize) = dumBfield(:,1:idumsize)
+    Bcons(:,1:idumsize) = dumBcons(:,1:idumsize)
+    dBconsdt(:,1:idumsize) = dumdBconsdt(:,1:idumsize)
+    fmag(:,1:idumsize) = dumfmag(:,1:idumsize)
+    curlB(:,1:idumsize) = dumcurlB(:,1:idumsize)
+    xsphterm(:,1:idumsize) = dumxsphterm(:,1:idumsize)
+        
+    rho(1:idumsize) = dumrho(1:idumsize)
+    drhodt(1:idumsize) = dumdrhodt(1:idumsize)
+    uu(1:idumsize) = dumuu(1:idumsize)
+    dudt(1:idumsize) = dumdudt(1:idumsize)
+    en(1:idumsize) = dumen(1:idumsize)
+    dendt(1:idumsize) = dumdendt(1:idumsize)
+    alpha(1:idumsize) = dumalpha(1:idumsize)
+    daldt(1:idumsize) = dumdaldt(1:idumsize)
+    hh(1:idumsize) = dumhh(1:idumsize)
+    dhdt(1:idumsize) = dumdhdt(1:idumsize)
+    gradh(1:idumsize) = dumgradh(1:idumsize)
+    pr(1:idumsize) = dumpr(1:idumsize)
+    spsound(1:idumsize) = dumspsound(1:idumsize)
+!    ll(1:idumsize) = dumll(1:idumsize)
+    divB(1:idumsize) = dumdivB(1:idumsize) 
+    ireal(1:idumsize) = idumireal(1:idumsize)
+    itype(1:idumsize) = idumitype(1:idumsize)
+    IF (ALLOCATED(fgrav)) fgrav(:,1:idumsize) = dumfgrav(:,1:idumsize)
+    sqrtg(1:idumsize) = dumsqrtg(1:idumsize)
+    dens(1:idumsize) = dumdens(1:idumsize)    
+    pmom(:,1:idumsize) = dumpmom(:,1:idumsize)    
+    IF (ALLOCATED(sourceterms)) THEN
+       sourceterms(:,1:idumsize) = dumsourceterms(:,1:idumsize)
+    ENDIF
+ ELSE
+    itype(:) = 0	! on first memory allocation, set all parts = normal
+ ENDIF   
+       
+!
+!--debugging information
+!
+ IF (trace) WRITE(iprint,*) '  Memory allocated, size(x,v)=',SIZE(x),SIZE(vel) 
+ 
+ RETURN
+
+END SUBROUTINE alloc
---
+++ src/stepND_mhd_gr.f90
@@ -0,0 +1,172 @@
+!!--------------------------------------------------------------------
+!! Computes one timestep
+!! Change this subroutine to change the timestepping algorithm
+!!--------------------------------------------------------------------
+	 
+SUBROUTINE step
+ USE dimen_mhd
+ USE debug
+ USE loguns
+ 
+ USE bound
+ USE eos
+ USE hterms
+ USE options
+ USE part
+ USE part_in
+ USE rates
+ USE timestep
+ USE setup_params
+ USE xsph
+!
+!--define local variables
+!
+ IMPLICIT NONE
+ INTEGER :: i,j,jdim,ikernavprev,ierr
+ REAL :: hdt
+!
+!--allow for tracing flow
+!      
+ IF (trace) WRITE(iprint,*) ' Entering subroutine step'
+!
+!--set initial quantities
+!
+ hdt = 0.5*dt
+ DO i=1,npart
+    xin(:,i) = x(:,i)
+    pmomin(:,i) = pmom(:,i)
+    Bconsin(:,i) = Bcons(:,i)
+    rhoin(i) = rho(i)
+    hhin(i) = hh(i)
+    enin(i) = en(i)
+    alphain(i) = alpha(i)
+ ENDDO   
+!
+!--Mid-point Predictor step
+!      
+ DO i=1,npart
+    IF (itype(i).EQ.1) THEN	! fixed particles
+       pmom(:,i) = pmomin(:,i)
+       rho(i) = rhoin(i)
+       Bcons(:,i) = Bconsin(:,i)	     
+       IF (iener.NE.0) en(i) = enin(i)
+       hh(i) = hhin(i)	    
+       x(:,i) = xin(:,i) + hdt*(vel(1:ndim,i) + xsphfac*xsphterm(1:ndim,i))
+       alpha(i) = alphain(i)
+    ELSE
+       x(:,i) = xin(:,i) + hdt*(vel(1:ndim,i) + xsphfac*xsphterm(1:ndim,i))
+       pmom(:,i) = pmomin(:,i) + hdt*force(:,i)
+       IF (imhd.NE.0) Bcons(:,i) = Bconsin(:,i) + hdt*dBconsdt(:,i)
+       IF (ihvar.EQ.1) THEN
+!	   hh(i) = hfact(pmass(i)/rho(i))**hpower	! my version
+	  hh(i) = hhin(i)*(rhoin(i)/rho(i))**hpower		! Joe's	   
+       ELSEIF (ihvar.EQ.2 .OR. ihvar.EQ.3) THEN
+          hh(i) = hhin(i) + hdt*dhdt(i)
+       ENDIF
+       IF (icty.GE.1) rho(i) = rhoin(i) + hdt*drhodt(i)
+       IF (iener.NE.0) en(i) = enin(i) + hdt*dendt(i)
+       IF (iavlim.NE.0) alpha(i) = alphain(i) + hdt*daldt(i)	   
+    ENDIF
+!
+!--for periodic boundaries, allow particles to cross the domain
+!  (this is only temporary as it is for the predicted quantity)
+!    
+    IF (ANY(ibound.EQ.3)) THEN
+       DO jdim=1,ndim
+          IF (ibound(jdim).EQ.3) THEN	! if periodic in this dimension
+	     IF (x(jdim,i).GT.xmax(jdim)) THEN
+!	        print*,' xold,xmax,xnew = ',i,x(jdim,i),xmax(jdim),xmin(jdim) + x(jdim,i) - xmax(jdim)
+	        x(jdim,i) = xmin(jdim) + x(jdim,i) - xmax(jdim)
+	     ELSEIF(x(jdim,i).LT.xmin(jdim)) THEN
+!	        print*,' xold,xmin,xnew = ',i,x(jdim,i),xmin(jdim),xmax(jdim) + x(jdim,i) - xmin(jdim)	     
+                x(jdim,i) = xmax(jdim) - (xmin(jdim) - x(jdim,i))
+             ENDIF	  
+	  ENDIF
+       ENDDO
+    ENDIF	 
+
+ ENDDO
+
+!
+!--set ghost particles if ghost boundaries are used
+!	 
+ IF (ANY(ibound.GE.2)) CALL set_ghost_particles
+!
+!--call link list to find neighbours
+!
+ CALL link
+!
+!--calculate density by direct summation
+!
+ IF (icty.LE.0) CALL iterate_density
+!
+!--calculate primitive variables from conservative variables
+!   
+ CALL conservative2primitive
+!
+!--calculate forces/rates of change using predicted quantities
+!	 
+ CALL get_rates
+!
+!--Mid-point Corrector step
+!
+ DO i=1,npart
+    IF (itype(i).EQ.1) THEN
+       pmom(:,i) = pmomin(:,i)
+       rho(i) = rhoin(i)
+       Bcons(:,i) = Bconsin(:,i)
+       IF (iener.NE.0) en(i) = enin(i)
+       x(:,i) = xin(:,i) + hdt*(vel(1:ndim,i) + xsphfac*xsphterm(1:ndim,i)) 
+       alpha(i) = alphain(i)
+       hh(i) = hhin(i)
+    ELSE
+       x(:,i) = xin(:,i) + dt*(vel(1:ndim,i) + xsphfac*xsphterm(1:ndim,i))
+       pmom(:,i) = pmomin(:,i) + dt*force(:,i)
+       IF (ihvar.EQ.2) THEN
+          hh(i) = hhin(i) + dt*dhdt(i)
+	  IF (hh(i).LE.0.) THEN
+	     WRITE(iprint,*) 'step: hh -ve ',i,hh(i)
+	     CALL quit
+	  ENDIF
+       ENDIF
+       IF (icty.GE.1) THEN
+          rho(i) = rhoin(i) + dt*drhodt(i)
+          IF (rho(i).LE.0.) THEN
+             WRITE(iprint,*) 'step: rho -ve ',i,rho(i)
+    	     CALL quit
+          ENDIF
+       ENDIF
+       IF (iener.NE.0) en(i) = enin(i) + dt*dendt(i)
+       IF (iavlim.NE.0) alpha(i) = alphain(i) + dt*daldt(i)	   
+       IF (imhd.NE.0) Bcons(:,i) = Bconsin(:,i) + dt*dBconsdt(:,i)	  
+    ENDIF
+ ENDDO	 	
+!
+!--update density using a full summation every so often
+!	 
+ IF (MOD(nsteps,ndirect).EQ.0) THEN
+    CALL iterate_density
+    DO i=1,npart
+       rhoin(i) = rho(i)
+    ENDDO
+ ENDIF
+!
+!--if doing divergence correction then do correction to magnetic field
+! 
+! IF (idivBzero.NE.0) CALL divBcorrect
+!
+ IF (ANY(ibound.NE.0)) CALL boundary	! inflow/outflow/periodic boundary conditions
+!
+!--set new timestep from courant/forces condition
+!
+ dt = min(C_force*dtforce,C_cour*dtcourant)
+!
+!--need to calculate velocity from particle momentum for next predictor step
+!  (no need to call this from output if called here)
+! 
+ CALL conservative2primitive
+  
+ IF (trace) WRITE (iprint,*) ' Exiting subroutine step'
+      
+ RETURN
+END SUBROUTINE step
