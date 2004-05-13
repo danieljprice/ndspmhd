f01b86ebf6ff00f79167701d5f76d027ff588e60
dprice <dprice>
2004-05-13 13:06:40 +0000

    minor crap

--- scripts/mvdat.sh
+++ scripts/mvdat.sh
@@ -7,16 +7,18 @@ if [ "$#" -ne 2 ]; then
 else
    runname=$1;
    newname=$2;
+   if [ -f $runname.ev ]; then echo copying to $newname.in; cp $runname.in $newname.in;
+   else echo no file $runname.in; fi   
    if [ -f $runname.ev ]; then echo $newname.ev; mv $runname.ev $newname.ev;
    else echo no file $runname.ev; fi
-   if [ -f $runname.dat ]; then echo mv $runname.dat $newname.dat; 
+   if [ -f $runname.dat ]; then echo $newname.dat; mv $runname.dat $newname.dat; 
    else echo no file $runname.dat; fi
-   if [ -f $runname.log ]; then echo mv $runname.log $newname.log;
+   if [ -f $runname.log ]; then echo $newname.log; mv $runname.log $newname.log;
    else echo no file $runname.log; fi
-   if [ -f $runname.sge ]; then echo mv $runname.sge $newname.sge;
+   if [ -f $runname.sge ]; then echo $newname.sge; mv $runname.sge $newname.sge;
    else echo no file $runname.sge; fi
-   if [ -f $runname.output ]; then echo mv $runname.output $newname.output;
+   if [ -f $runname.output ]; then echo $newname.output; mv $runname.output $newname.output;
    else echo no file $runname.output; fi
-   if [ -f $runname.errors ]; then echo mv $runname.errors $newname.errors;
+   if [ -f $runname.errors ]; then echo $newname.errors; mv $runname.errors $newname.errors;
    else echo no file $runname.errors; fi
 fi
