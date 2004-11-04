078921e7babebf14658354f9146793e2b74a57ff
dprice <dprice>
2004-11-04 09:19:46 +0000

    external forces cleaned up, should be more robust

--- src/external_forces.f90
+++ src/external_forces.f90
@@ -2,16 +2,12 @@
 !!
 !! Computes external (body) forces on a particle given its co-ordinates
 !!
-!! Note that the potential energies for these forces are calculated in
-!! evwrite and should correspond to the options used in this subroutine
-!!
 !!-----------------------------------------------------------------------
-subroutine external_forces(ipart,iexternal_force)
-  use dimen_mhd
-  use part
-  use rates
+subroutine external_forces(iexternal_force,xpart,fext,ndim)
   implicit none
-  integer, intent(in) :: ipart,iexternal_force
+  integer, intent(in) :: iexternal_force,ndim
+  real, dimension(ndim), intent(in) :: xpart
+  real, dimension(ndim), intent(out) :: fext
   real, dimension(ndim) :: dr
   real :: rr,rr2,drr2
 
@@ -20,36 +16,69 @@ subroutine external_forces(ipart,iexternal_force)
 !
 !--toy star force (linear in co-ordinates)
 !
-     force(1:ndim,ipart) = force(1:ndim,ipart) - x(1:ndim,ipart)
+     fext(:) = - xpart(:)
      
   case(2)
 !
 !--1/r^2 force from central point mass
 !
-     rr2 = DOT_PRODUCT(x(:,ipart),x(:,ipart)) 
+     rr2 = DOT_PRODUCT(xpart,xpart) 
      rr = SQRT(rr2)
      drr2 = 1./rr2
-     dr(:) = x(:,ipart)/rr
-     force(1:ndim,ipart) = force(1:ndim,ipart) - dr(1:ndim)*drr2
+     dr(:) = xpart(:)/rr
+     fext(:) = - dr(:)*drr2
 
   case(3)
 !
 !--compute force on gas particle from n point masses
 !
+     fext = 0.
 !     do i=1,nptmass
         !
         !--work out distance to point mass
         !        
-!        dr(:) = xptmass(:,i) - x(:,ipart)
+!        dr(:) = xptmass(:,i) - xpart(:)
 !        rr2 = DOT_PRODUCT(dr,dr)
 !        drr2 = 1./rr2
 !        rr = SQRT(rr2)
 !        dr(:) = dr(:)/rr
         !
         !--calculate gravitational force
-        !  (need to calculate fgrav separately for operator splitting)
         !
-!        force(1:ndim,ipart) = force(1:ndim,ipart) - dr(1:ndim)*drr2
+!        fext(:) = fext(:) - dr(:)*drr2
 !     enddo
+  case default
+     
+     fext(:) = 0.
+     
   end select
+  
+  return
 end subroutine external_forces
+
+!!-----------------------------------------------------------------------
+!!
+!! Calculate potential energy for above forces
+!! (called from evwrite in adding up total energy)
+!!
+!!-----------------------------------------------------------------------
+
+subroutine external_potentials(iexternal_force,xpart,epot,ndim)
+ implicit none
+ integer, intent(in) :: iexternal_force,ndim
+ real, dimension(ndim), intent(in) :: xpart
+ real, intent(out) :: epot
+ 
+ select case(iexternal_force)
+ case(1) ! toy star force (x^2 potential)
+    epot = 0.5*DOT_PRODUCT(xpart,xpart)
+ case(2) ! 1/r^2 force(1/r potential)
+    epot = 1./SQRT(DOT_PRODUCT(xpart,xpart))
+ case(3) ! potential from n point masses
+    epot = 0.
+ case default
+    epot = 0.
+ end select
+
+ return
+end subroutine external_potentials
