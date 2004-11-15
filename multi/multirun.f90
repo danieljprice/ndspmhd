400a0dde1b0e3c921b980b7ff8d2c754bcb00874
dprice <dprice>
2004-11-15 15:29:07 +0000

    varying sigma, hfact, psep

--- multi/multirun.f90
+++ multi/multirun.f90
@@ -1,7 +1,7 @@
 !!-----------------------------------------------------------------------
 !! computes multiple infiles where one or several parameters are varied
 !!
-!! this one does Brio/Wu shock test, varying the div B cleaning parameters
+!! this varies h and the div B cleaning parameters
 !!-----------------------------------------------------------------------
 PROGRAM multirun
  USE dimen_mhd
@@ -16,10 +16,7 @@ PROGRAM multirun
  IMPLICIT NONE
  INTEGER :: i,j,nruns,iener_default
  INTEGER :: int_from_string
- CHARACTER :: filename*15,infile*20,shkfile*20,filenum*2,charnruns*3
- REAL :: rholeft,rhoright,prleft,prright
- REAL :: vxleft,vxright,vyleft,vyright,vzleft,vzright
- REAL :: Bxinit,Byleft,Byright,Bzleft,Bzright,const
+ CHARACTER :: filename*15,infile*20,filenum*2,charnruns*3
  
  nruns = 0
  iread = 11
@@ -47,59 +44,28 @@ PROGRAM multirun
  PRINT*,' initial psep = ',psep
  iener_default = iener
  
- const = 1./SQRT(4.*pi)
  psidecayfact = -0.1
+ hfact = 1.0
+ j = 0
  
  DO i=1,nruns
     IF (i.GE.10) THEN
        filenum = ACHAR(48+i/10)//ACHAR(48+mod(i,10))
        infile = TRIM(filename)//filenum//'.in'
-       shkfile = TRIM(filename)//filenum//'.shk'
     ELSE
        filenum(1:1) = ACHAR(48+mod(i,10))
        infile = TRIM(filename)//filenum(1:1)//'.in'
-       shkfile = TRIM(filename)//filenum(1:1)//'.shk'
     ENDIF
-    PRINT*,' writing input file ',infile, ' psep = ',psep
     
-!
-!--brio/wu problem
-!
-       psep = 0.0056
-       gamma = 2.0
-       tmax = 0.1
-       tout = 0.05
-       imhd = 1
-       idivBzero = 2
-       psidecayfact = psidecayfact + 0.1
-
-       rholeft = 1.0
-       rhoright = 0.125
-       prleft = 1.0
-       prright = 0.1
-       vxleft = 0.
-       vxright = 0.
-       vyleft = 0.
-       vyright = 0.
-       vzleft = 0.
-       vzright = 0.
-       Bxinit = 0.75
-       Byleft = 1.0
-       Byright = -1.0
-       Bzleft = 0.
-       Bzright = 0.     
+    j = j + 1
+    IF (j.GT.9) THEN
+       j = 0
+       psep = 0.5*psep
+       psidecayfact = -0.1
+    ENDIF
+    psidecayfact = psidecayfact + 0.1
     
-    PRINT*,'Writing ',shkfile,' with initial left/right states'
-    OPEN(UNIT=11,FILE=shkfile,STATUS='replace',FORM='formatted')
-       WRITE(11,*) rholeft,rhoright
-       WRITE(11,*) prleft,prright
-       WRITE(11,*) vxleft,vxright
-       WRITE(11,*) vyleft,vyright              
-       WRITE(11,*) vzleft,vzright
-       WRITE(11,*) Bxinit
-       WRITE(11,*) Byleft,Byright
-       WRITE(11,*) Bzleft,Bzright       
-    CLOSE(UNIT=11)
+    PRINT*,'Writing input file ',infile, ' psep = ',psep,' sigma = ',psidecayfact
     
     CALL write_infile(infile)
 
