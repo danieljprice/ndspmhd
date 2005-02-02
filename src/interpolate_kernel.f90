4507850dd36e0d29de85c60acd1b528982a39d4d
dprice <dprice>
2005-02-02 01:21:47 +0000

    bug fix (index1 checking)

--- src/interpolate_kernel.f90
+++ src/interpolate_kernel.f90
@@ -18,7 +18,7 @@ SUBROUTINE interpolate_kernel(q2,W,gradW)
  index = INT(q2*ddq2table)
  index1 = index + 1
  IF (index.GT.ikern .or. index.LT.0) index = ikern
- IF (index1.GT.ikern .or. index.LT.0) index1 = ikern
+ IF (index1.GT.ikern .or. index1.LT.0) index1 = ikern
 !
 !--find increment from index point to actual value of q2
 !
