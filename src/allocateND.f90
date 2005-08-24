e09909aa29bc905e41d8656b63ac6e7e7ede0da3
dprice <dprice>
2005-08-24 13:56:43 +0000

    bug fix with mem allocation if using ghosts

--- src/allocateND.f90
+++ src/allocateND.f90
@@ -63,9 +63,9 @@ SUBROUTINE alloc(newsizein)
     STOP
  ENDIF
 !
-!--set new size (add 10% so don't have to do this too often)
+!--set new size (add 10% if using ghosts so don't have to do this too often)
 !
- IF (ANY(ibound.GE.1) .AND. newsizein.GT.ioldsize) THEN
+ IF (ANY(ibound.GE.1) .AND. newsizein.GE.ioldsize) THEN
     newsize = INT(1.1*newsizein)
  ELSEIF (newsizein.EQ.ioldsize) THEN
     WRITE(iprint,*) 'allocate: old size = new size: doing nothing'
