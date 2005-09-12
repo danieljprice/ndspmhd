9842bbe48bea8a04f0d3235242aef2f03cb984dd
dprice <dprice>
2005-09-12 08:35:35 +0000

    2D vector potential implemented (two first derivs only)

--- src/get_curl.f90
+++ src/get_curl.f90
@@ -3,53 +3,50 @@
 !! This version computes the curl for all particles
 !! and therefore only does each pairwise interaction once
 !!------------------------------------------------------------------------
+module getcurl
+ implicit none
+ 
+contains
 
-subroutine get_curl(curlbonrho,ntot)
- use dimen_mhd, only:ndim,ndimv
+subroutine get_curl(npart,x,pmass,rho,hh,Bvec,curlB)
+ use dimen_mhd, only:ndim,ndimV,idim
  use debug, only:trace
  use loguns, only:iprint
  
- use bound
  use kernels, only:interpolate_kernel,radkern2
- use linklist
- use options, only:ikernav,imhd
- use part
- use setup_params
- use get_neighbour_lists
+ use linklist, only:ll,ifirstincell,ncellsloop
+ use get_neighbour_lists, only:get_neighbour_list
+ use hterms, only:gradh
 !
 !--define local variables
 !
  implicit none
- integer, intent(in) :: ntot
+ integer, intent(in) :: npart
+ real, dimension(ndim,idim), intent(in) :: x
+ real, dimension(idim), intent(in) :: pmass,rho,hh
+ real, dimension(ndimV,idim), intent(in) :: Bvec
+ real, dimension(ndimV,idim), intent(out) :: curlB
+
  integer :: i,j,n
  integer :: icell,iprev,nneigh
- integer, dimension(ntot) :: listneigh ! neighbour list
+ integer, dimension(npart) :: listneigh ! neighbour list
  integer :: idone
-!
-!  (particle properties - local copies)
-!      
  real :: rij,rij2
- real :: hi,hi1,hav,hav1,hj,hj1,h2,hi2,hj2
- real :: hfacwab,hfacwabi,hfacwabj
- real :: rho21i, rho21j, pmassi
+ real :: hi,hi1,hj,hj1,hi2,hj2
+ real :: hfacwabi,hfacwabj
+ real :: pmassi
  real, dimension(ndim) :: dx
- real, dimension(ndimv) :: dr, db, bi, bj, curlbi
- real, dimension(ndimv,ntot), intent(out) :: curlbonrho
-!
-!  (kernel quantities)
-!
- real :: q2,q2i,q2j      
- real :: wab,wabi,wabj
- real :: grkern,grkerni,grkernj
+ real, dimension(ndimV) :: dr, dB, Bi, curlBi
+ real :: q2i,q2j,wabi,wabj,grkerni,grkernj
 !
 !--allow for tracing flow
 !      
- if (trace) write(iprint,*) ' entering subroutine get_curl, ntot=',ntot  
+ if (trace) write(iprint,*) ' entering subroutine get_curl' 
 !
 !--initialise quantities
 !
  listneigh = 0
- curlbonrho = 0.
+ curlB = 0.
 !
 !--loop over all the link-list cells
 !
@@ -74,13 +71,8 @@ subroutine get_curl(curlbonrho,ntot)
        hi1 = 1./hi
        hi2 = hi*hi
        hfacwabi = hi1**ndim
-       rho21i = 1./rho(i)**2
        pmassi = pmass(i)
-       if (imhd.ge.11) then      ! if mag field variable is b
-          bi(:) = bevol(:,i)
-       elseif (imhd.gt.0) then      ! if mag field variable is b/rho
-          bi(:) = bfield(:,i)
-       endif
+       Bi(:) = Bvec(:,i)
 !
 !--for each particle in the current cell, loop over its neighbours
 !
@@ -95,73 +87,54 @@ subroutine get_curl(curlbonrho,ntot)
 !
 !--calculate averages of smoothing length if using this averaging
 !                
-             hav = 0.5*(hi + hj)
-             hav1 = 1./hav
-             h2 = hav*hav
-             hfacwab = hav1**ndim
              hfacwabj = hj1**ndim
-             rho21j = 1./rho(j)**2
              
              rij2 = dot_product(dx,dx)
              rij = sqrt(rij2)
-             q2 = rij2/h2
              q2i = rij2/hi2
              q2j = rij2/hj2     
              dr(1:ndim) = dx(1:ndim)/rij  ! unit vector
-             if (ndimv.gt.ndim) dr(ndim+1:ndimv) = 0. 
+             if (ndimV.gt.ndim) dr(ndim+1:ndimV) = 0. 
 
              !          print*,' neighbour,r/h,dx,hi,hj ',j,sqrt(q2),dx,hi,hj
 !     
 !--do interaction if r/h < compact support size
 !  don't calculate interactions between ghost particles
 !
-             if (((q2i.lt.radkern2).or.(q2j.lt.radkern2))  &
-                  .and. .not.(i.gt.npart.and.j.gt.npart)) then
+             if ((q2i.lt.radkern2).or.(q2j.lt.radkern2)) then
 !     
 !--interpolate from kernel table          
 !  (use either average h or average kernel gradient)
 !
                 !       print*,' neighbour,r/h,dx,hi,hj ',i,j,sqrt(q2),dx,hi,hj
-                if (ikernav.eq.1) then          
-                   call interpolate_kernel(q2,wab,grkern)
-                   wab = wab*hfacwab
-                   grkern = grkern*hfacwab*hj1
-                else
-                   !  (using hi)
-                   call interpolate_kernel(q2i,wabi,grkerni)
-                   wabi = wabi*hfacwabi
-                   grkerni = grkerni*hfacwabi*hi1
-                   !  (using hj)
-                   call interpolate_kernel(q2j,wabj,grkernj)
-                   wabj = wabj*hfacwabj
-                   grkernj = grkernj*hfacwabj*hj1
-                   !  (calculate average)            
-                   wab = 0.5*(wabi + wabj)                  
-                   grkern = 0.5*(grkerni + grkernj)            
-                endif
-
-                if (imhd.ge.11) then      ! if b is mag field variable
-                   bj(:) = bevol(:,j)
-                elseif (imhd.ne.0) then      ! if b/rho is mag field variable
-                   bj(:) = bfield(:,j)                                
-                endif
+                !  (using hi)
+                call interpolate_kernel(q2i,wabi,grkerni)
+                wabi = wabi*hfacwabi
+                grkerni = grkerni*hfacwabi*hi1
+                !  (using hj)
+                call interpolate_kernel(q2j,wabj,grkernj)
+                wabj = wabj*hfacwabj
+                grkernj = grkernj*hfacwabj*hj1
 !
-!--calculate div b
+!--calculate curl of Bvec
 !
-                db = bi(:) - bj(:)
-                if (ndimv.eq.3) then
-                   curlbi(1) = db(2)*dr(3) - db(3)*dr(2)
-                   curlbi(2) = db(3)*dr(1) - db(1)*dr(3)
-                   curlbi(3) = db(1)*dr(2) - db(2)*dr(1)
-                elseif (ndimv.eq.2) then  ! just jz in 2d
-                   curlbi(1) = db(1)*dr(2) - db(2)*dr(1)
-                   curlbi(2) = 0.
+                dB = Bi(:) - Bvec(:,j)
+                if (ndim.eq.3) then
+                   curlBi(1) = dB(2)*dr(3) - dB(3)*dr(2)
+                   curlBi(2) = dB(3)*dr(1) - dB(1)*dr(3)
+                   curlBi(3) = dB(1)*dr(2) - dB(2)*dr(1)
+                elseif (ndim.eq.2) then  ! just Az in 2d
+                   curlBi = 0.
+                   curlBi(1) = -dB(1)*dr(2) ! replace dB(3) by dB(1)
+                   curlBi(2) = dB(1)*dr(1)
+!                   curlBi(1) = db(1)*dr(2) - db(2)*dr(1)
+!                   curlBi(2) = 0.
                 endif
                 !
                 !--compute rho * current density j
                 !
-                curlbonrho(:,i) = curlbonrho(:,i) - pmass(j)*curlbi(:)*grkern
-                curlbonrho(:,j) = curlbonrho(:,j) - pmassi*curlbi(:)*grkern
+                curlB(:,i) = curlB(:,i) - pmass(j)*curlBi(:)*grkerni
+                curlB(:,j) = curlB(:,j) - pmassi*curlBi(:)*grkernj
                 !      else
                 !         print*,' r/h > 2 '      
                 
@@ -175,10 +148,11 @@ subroutine get_curl(curlbonrho,ntot)
     
  enddo loop_over_cells
 
- do i=1,ntot
-    curlbonrho(:,i) = curlbonrho(:,i)/rho(i)**2
+ do i=1,npart
+    curlB(:,i) = curlB(:,i)*gradh(i)/rho(i)
  enddo
 
  return
 end subroutine get_curl
       
+end module getcurl
--- src/setup_orszagtang2D_mhd.f90
+++ src/setup_orszagtang2D_mhd.f90
@@ -14,9 +14,9 @@ subroutine setup
 !
 !--include relevant global variables
 !
- use dimen_mhd
- use debug
- use loguns
+ use dimen_mhd, only:ndim,ndimV
+ use debug, only:trace
+ use loguns, only:iprint
  use bound
  use eos
  use options
@@ -28,21 +28,21 @@ subroutine setup
 !--define local variables
 !      
  implicit none
- integer :: i,j,ntot,npartx,nparty,ipart
+ integer :: ipart
  real :: betazero,denszero,przero,vzero,bzero,uuzero,machzero
  real :: totmass,gam1,massp,const
 !
 !--check number of dimensions is right
 !
- if (ndim.ne.2) stop ' ndim must be = 2 for orszag-tang vortex'
+ if (ndim.ne.2) stop ' ndim must be = 2 for Orszag-Tang vortex'
 !
 !--set boundaries
-!            	    
- ibound = 3	! periodic
- nbpts = 0	! must use fixed particles if inflow/outflow at boundaries
- xmin(1) = 0.0		! x
+!                        
+ ibound = 3     ! periodic
+ nbpts = 0      ! must use fixed particles if inflow/outflow at boundaries
+ xmin(1) = 0.0  ! x
  xmax(1) = 1.0
- xmin(2) = 0.0		! y
+ xmin(2) = 0.0  ! y
  xmax(2) = 1.0
 !
 !--setup parameters
@@ -54,28 +54,19 @@ subroutine setup
  bzero = 1.0/sqrt(const)
  przero = 0.5*bzero**2*betazero
  denszero = gamma*przero*machzero
-! denszero = 1.0
  
  gam1 = gamma - 1.
-! npartx = int((xmax(1)-xmin(1))/psep)
-! nparty = int((xmax(2)-xmin(2))/psep)
  uuzero = przero/(gam1*denszero)
 
- write(iprint,*) 'two dimensional orszag-tang vortex problem '
+ write(iprint,*) 'Two dimensional Orszag-Tang vortex problem '
  write(iprint,10) betazero,machzero,bzero,denszero,przero
 10 format(/,' beta        = ',f6.3,', mach number = ',f6.3,/, &
-            ' initial b   = ',f6.3,', density = ',f6.3,', pressure = ',f6.3,/)
+            ' initial B   = ',f6.3,', density = ',f6.3,', pressure = ',f6.3,/)
 !
-!--allocate memory here
-!
-! ntot = npartx*nparty
-! call alloc(ntot)
-! npart = ntot
-! ntotal = ntot
-!
-!--setup uniform density grid of particles (2d) with sinusoidal field/velocity
+!--setup uniform density grid of particles (2D) with sinusoidal field/velocity
 !  determines particle number and allocates memory
- call set_uniform_cartesian(2,psep,xmin,xmax)	! 2 = close packed arrangement
+!
+ call set_uniform_cartesian(2,psep,xmin,xmax)        ! 2 = close packed arrangement
 
  ntotal = npart
 !
@@ -93,15 +84,17 @@ subroutine setup
     dens(ipart) = denszero
     pmass(ipart) = massp
     uu(ipart) = uuzero
-    if (imhd.ge.1) then 
-       bfield(1,ipart) = -bzero*sin(2.*pi*x(2,ipart))
-       bfield(2,ipart) = bzero*sin(4.*pi*x(1,ipart))
-       if (ndimv.eq.3) bfield(3,ipart) = 0.0	
+    if (imhd.ge.1) then
+       Bfield(1,ipart) = -Bzero*sin(2.*pi*x(2,ipart))
+       Bfield(2,ipart) = Bzero*sin(4.*pi*x(1,ipart))
+       if (ndimv.eq.3) Bfield(3,ipart) = 0.0
+    elseif (imhd.lt.0) then
+!--vector potential setup
+       Bevol(:,ipart) = 0.
+       Bevol(1,ipart) = -0.5/pi*Bzero*(cos(2.*pi*x(2,ipart)) + 0.5*cos(4.*pi*x(1,ipart)))
     else
-       bfield(:,ipart) = 0.
+       Bfield(:,ipart) = 0.
     endif 
-!       print*,ipart,x(:,ipart),dens(ipart),uu(ipart),pmass(ipart)
-!       if (mod(i,1000).eq.0) read*
  enddo
 
 !
