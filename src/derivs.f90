fd5a91336ddb7c6e09f541d2bb84f6111266ac49
dprice <dprice>
2006-04-21 16:22:58 +0000

    timings added

--- src/derivs.f90
+++ src/derivs.f90
@@ -3,11 +3,16 @@
 !  (wrapper for call to density and rates, calls neighbours etc first)
 !
 subroutine derivs
+ use loguns, only:iprint
  use options, only:ibound,icty
  implicit none
+ logical :: itiming
+ real :: t1,t2,t3,t4,t5
 !
 !--allow particles to cross boundary (ie. enforce boundary conditions)
 !
+ itiming = .true.
+ if (itiming) call cpu_time(t1)
  if (ANY(ibound.NE.0)) call boundary	! inflow/outflow/periodic boundary conditions
 !
 !--set ghost particles if ghost boundaries are used
@@ -20,15 +25,29 @@ subroutine derivs
 !
 !--calculate density by direct summation
 !
+ if (itiming) call cpu_time(t2)
  if (icty.LE.0) call iterate_density
 !
 !--calculate primitive variables from conservative variables
-!   
+!
+ if (itiming) call cpu_time(t3) 
  call conservative2primitive
 !
 !--calculate forces/rates of change using predicted quantities
 !
+ if (itiming) call cpu_time(t4)
+
  call get_rates
 
+ if (itiming) then
+    call cpu_time(t5)
+    write(iprint,"(50('-'))") 
+    write(iprint,*) 'time for dens    = ',t3-t2,'s'
+    write(iprint,*) 'time for rates   = ',t5-t4,'s'
+    write(iprint,*) '        others   = ',(t2-t1)+(t4-t3),'s'
+    write(iprint,*) 'total for derivs = ',t5-t1,'s'
+    write(iprint,"(50('-'))") 
+ endif
+ 
  return
 end subroutine derivs
