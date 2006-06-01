310f72e41a52cbbe53db5d5c28f27bb12776cb7c
dprice <dprice>
2006-06-01 14:06:51 +0000

    xgrid changes, writemake uses ~/ndspmhd

--- scripts/domulti_xgrid.pl
+++ scripts/domulti_xgrid.pl
@@ -12,6 +12,7 @@ my $zero = 0;
 my $ext = '.dat';
 my $dir = `pwd`;
 my $SPMHD = 'SPMHD';
+my $ndspmhddir = '~/ndspmhd';
 
 if ($#ARGV <2) {
    die "Usage: $0 nD rootname nruns [nstartfrom]\n";
@@ -19,27 +20,32 @@ if ($#ARGV <2) {
 
 my ($ndim,$rootname,$nruns,$nstart) = @ARGV;
 $nstart = 1 unless defined $nstart; ## number to start from (either zero or 1)
-my $rundir = "$dir'/'$rootname";
+my $rundir = "$dir/$rootname";
+print "rundir is $rundir \n";
 
 # make a new directory for the run
-print "making new directory $rootname";
-system "mkdir $rootname";
+print "making new directory $rootname \n";
+system "mkdir $rootname" || die "error creating run directory";
+
 # copy files to this directory
-system "cp multirun.in ./$rootname";
-system "cp $ndim$SPMHD ./$rootname";
-system "cp defaults ./$rootname";
-system "cd $rootname; ../scripts/writemake.tcsh $ndim > Makefile";
+system "cp multirun.in ./$rootname" || die "ERROR: multirun.in not found/not readable";
+system "cp $ndspmhddir/$ndim$SPMHD ./$rootname";
+system "cd $rootname; $ndspmhddir/scripts/writemake.tcsh $ndim > Makefile";
 
 # call the multirun program to generate the input files
-system "cd $rootname; ~/ndspmhd/multi/multirun $rootname $nruns";
+system "cd $rootname; $ndspmhddir/multi/multirun $rootname $nruns";
 
 # write appropriate runnames to 'runname' and execute program
 for ($n = $nstart;$n<=$nruns;$n++) {
     print "doing run $rootname$n \n";
 ##    print "copying $rootname\_init.dat $rootname$n\_init.dat \n";
 ##    system "cd $rootname; cp $rootname\_init.dat $rootname$n\_init.dat; ./$ndim$SPMHD $rootname$n\_init.dat > $rootname$n.output";
-    ##system "echo 'cd $dir ./$ndim$SPMHD $rootname$n ' > $rootname/$rootname$n.csh";
-    print "cd $rootname; xgrid -hostname cytosine.ex.ac.uk -auth Kerberos -in \$PWD -job submit ./$ndim$SPMHD $rootname$n";
+    my $jobid = `cd $rootname; xgrid -hostname cytosine.ex.ac.uk -auth Kerberos -in \$PWD -out \$PWD -se \$PWD/$rootname$n.errors -so \$PWD/$rootname$n.output -job submit ./$ndim$SPMHD $rootname$n`;
+    print "$jobid \n";
+    ($jobid) = $jobid =~ m/jobIdentifier\s+=\s+(\d+);/; # \s matches spaces (+ = at least one) \d decimals
+    print "job id = $jobid \n";
+    system "cd $rootname; echo xgrid -hostname cytosine.ex.ac.uk -auth Kerberos -out \$PWD -se \$PWD/$rootname$n.errors -so \$PWD/$rootname$n.output -job results -id $jobid >> getresults";
+    system "cd $rootname; echo xgrid -hostname cytosine.ex.ac.uk -auth Kerberos -job delete -id $jobid >> cleanup";
 }
 
 # call the sametime program to write simultaneous data steps
