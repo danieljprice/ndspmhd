f912a3cc261d98f0ffad17dd99f8bb887657ecb8
dprice <dprice>
2006-09-25 08:56:17 +0000

    added improvements

--- scripts/xgridq.pl
+++ scripts/xgridq.pl
@@ -12,12 +12,23 @@ my $format=" %4u | %25s | %9s | %25s \n";
 my $formath=" %4s | %25s | %9s | %25s \n";
 printf $formath,'ID','Script name','Status','Date Submitted';
 
+my $nrun = 0;
+my $nfinished = 0;
+my $npending = 0;
 foreach (@joblist) {
    my ($jobid) = m/(\d+)/;  
    my $attributes = `xgrid $xgridauth -job attributes -id $jobid`;
    my ($status) = $attributes =~ m/jobStatus = (\w+);/;
    my ($name) = $attributes =~ m/name = "(\S+)";/;
    my ($submitted) = $attributes =~ m/dateSubmitted = (.+);/;
+   if ($status eq 'Running') {
+      $nrun = $nrun + 1;
+   } elsif ($status eq 'Finished') {
+      $nfinished = $nfinished + 1;
+   } elsif ($status eq 'Pending') {
+      $npending = $npending + 1;
+   }
 #   print "$attributes \n";
    printf $format, $jobid, $name, $status, $submitted;
 }
+print " $nrun jobs running; $npending jobs pending; $nfinished jobs finished\n";
