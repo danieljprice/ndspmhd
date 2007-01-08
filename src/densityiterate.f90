d46cf92bf3fc801535030face222a42d21370e01
dprice <dprice>
2007-01-08 14:20:36 +0000

    gradh added

--- src/densityiterate.f90
+++ src/densityiterate.f90
@@ -23,14 +23,14 @@ subroutine densityiterate
  use rates, only:dhdt,drhodt
  use setup_params, only:hfact,psep
  use get_neighbour_lists, only:get_neighbour_list_partial
- use kernels, only:interpolate_kernel_dens,radkern,radkern2
+ use kernels, only:interpolate_kernel,radkern,radkern2
  implicit none
  real, parameter :: pi = 3.1415926536
  integer :: i,j,n,icell,icellprev,nneigh,nneighi,itsdensity
  integer :: minneigh,maxneigh,ncalc,nitsmax
  integer, dimension(ntotal) :: listneigh
- real :: const,sum1,sum2,hi,hi21,hnew
- real :: func0,func,dfunc
+ real :: const,sum1,sum2,dwdhterm1,hi,hi1,hi21,hnew,dwdhi
+ real :: func0,func,dfunc,dfdh,dhdrhoi,omegai,rhomin,rhoi
  real, dimension(ndim) :: dx
  real :: rij2,q2i,pmassj,wabi,grkerni,grgrkerni
  logical :: converged
@@ -50,7 +50,7 @@ subroutine densityiterate
     const = 0.3*radkern2
  end select
  const = 0.
- print*,'const = ',const,'hhmax=',hhmax
+ rhomin = 0.
  icellprev = 0
  minneigh = huge(minneigh)
  maxneigh = 0
@@ -83,18 +83,23 @@ subroutine densityiterate
 !--if different to previous cell used, get the list of neighbours for this cell
 !  (common to all particles in the cell)
 !
-       if (hi.gt.hhmax) then
+       if (hh(i).gt.hhmax) then
           if (any(ibound.gt.1)) call set_ghost_particles
-          write(iprint,*) 'relinking...'
+          write(iprint,*) 'relinking...hi = ',hi,hhmax
           call set_linklist
+          icell = iamincell(i)
           call get_neighbour_list_partial(icell,listneigh,nneigh)
+          write(iprint,*) 'new nneigh = ',nneigh
        elseif (icell.NE.icellprev) then
           call get_neighbour_list_partial(icell,listneigh,nneigh)
        endif
        icellprev = icell
        sum1 = 0.
        sum2 = 0.
-       hi21 = 1./hi**2
+       dwdhterm1 = 0.
+       hi = hh(i)
+       hi1 = 1./hi
+       hi21 = hi1**2
        nneighi = 0
 !
 !--loop over current particle's neighbours
@@ -113,46 +118,61 @@ subroutine densityiterate
 !      
 !--interpolate from kernel table (using hi)
 !
-             call interpolate_kernel_dens(q2i,wabi,grkerni,grgrkerni)             
+             call interpolate_kernel(q2i,wabi,grkerni)             
              sum1 = sum1 + pmassj*wabi
-             sum2 = sum2 + pmassj*grgrkerni
-             if (i.ne.j) sum2 = sum2 + pmassj*(ndim-1)*grkerni/sqrt(q2i)
+             dwdhterm1 = dwdhterm1 - pmassj*grkerni*sqrt(q2i)
+!             sum2 = sum2 + pmassj*grgrkerni
+!             if (i.ne.j) sum2 = sum2 + pmassj*(ndim-1)*grkerni/sqrt(q2i)
 !                print*,'q2i,w = ',q2i,wabi,grgrkerni
           endif
        enddo loop_over_neighbours
 !
 !--work out sums, see if h is converged
 !
-       func = sum1 + const*sum2
-!       print*,'sum1, sum2 = ',sum1,const*sum2
+       func = sum1 !!+ const*sum2
+       dfdh = dwdhterm1*hi1
+       print*,i,'iteration',itsdensity,' rho = ',sum1*hi1**ndim,'h = ',hi
+       print*,'  nneigh = ',nneighi, 'dfdh = ',dfdh
        dfunc = func - func0
        
        converged = (abs(dfunc/func) < tolh) .or. itsdensity.ge.maxdensits
+
+          rho(i) = sum1*hi1**ndim
+          rhoi = pmass(i)/(hh(i)/hfact)**ndim - rhomin ! this is the rho compatible with the old h
+          dwdhi = dwdhterm1*hi1**(ndim+1) - ndim*hi1*rhoi
+          dhdrhoi = -hi/(ndim*(rhoi + rhomin))
+          omegai = 1. - dhdrhoi*dwdhi
+          gradh(i) = 1./omegai   ! this is what *multiplies* the kernel gradient in rates etc
+          func = rhoi - rho(i)
+          dfdh = omegai/dhdrhoi
+          hnew = hh(i) - func/dfdh
+          converged = (abs((hnew-hh(i))/hi) < tolh .and. omegai > 0.)
+
+       if (nneighi.le.1) then
+          !print*,'NO NEIGHBOURS : rho = ',rho(i),' h = ',hnew,hh(i)
+          print*,' WARNING: particle ',i,' has no neighbours, increasing h'
+          hnew = hh(i) + psep
+          converged = .false.
+       endif
 !
 !--set h for next iterations if not converged
 !
        if (.not.converged) then
+!
+!--perform Newton-Raphson iteration to get new h
+!
+
+!          hnew = hi - func/dfdh
           !--fixed point iteration
-!          print*,i,'hold = ',hi,' neigh = ',nneighi,'rho = ',sum1/(hi**ndim)
-!          print*,'del2rho =',sum2/(hi**(ndim+2))
-!          print*,'fact=',hfact*(pmass(i)/(sum1 + const*sum2))**dndim
-          hi = hfact*(pmass(i)/(sum1 + const*sum2))**dndim*hi
-!          select case(ndim)
-!          case(1)
-!          print*,'Neigh 1 =',2.*radkern*sum1/pmass(i)
-!          print*,'Neigh 2 =',(2.*radkern*sum1 + radkern**3*2./3.*sum2)/pmass(i)
-!          case(3)
-!          print*,'Neigh 1 =',4./3.*pi*radkern**3*sum1/pmass(i)
-!          print*,'Neigh 2 =',(4./3.*pi*radkern**3*sum1 + 2.*pi/5.*radkern**5*sum2)/pmass(i)
-!          end select
+          print*,'h new = ',hnew,' func/dfdh = ',func/dfdh
+!          if (hnew.lt.0.) then
+!             write(iprint,*) 'taking fixed point, h = ',hfact*(pmass(i)/sum1)**dndim*hi,hi - func/dfdh
+!             hnew = hfact*(pmass(i)/sum1)**dndim*hi
+!          endif
+          hh(i) = hnew
 !          read*
        endif
-       if (nneighi.le.1) then
-          !print*,'NO NEIGHBOURS : rho = ',rho(i),' h = ',hnew,hh(i)
-          print*,' WARNING: particle ',i,' has no neighbours, increasing h'
-          hi = hi + psep
-          converged = .false.
-       endif
+
 
     enddo iterate
 
@@ -161,12 +181,16 @@ subroutine densityiterate
 !
 !--having finished iterations, now calculate extra crap
 !
-    rho(i) = sum1/(hi**ndim)
+    rhoi = sum1*hi1**ndim
+    rho(i) = sum1*hi1**ndim
     hh(i) = hi
+    dwdhi = dwdhterm1*hi1**(ndim+1) - ndim*hi1*rhoi
+    dhdrhoi = -hi/(ndim*(rho(i) + rhomin))
+    omegai = 1. - dhdrhoi*dwdhi
+    gradh(i) = 1./omegai
+    gradsoft(i) = 0.    
     drhodt(i) = 0.
     dhdt(i) = 0.
-    gradh(i) = 1.
-    gradsoft(i) = 0.
     minneigh = min(minneigh,nneighi)
     maxneigh = max(maxneigh,nneighi)
 !    print*,i,'its = ',itsdensity,'neigh = ',nneighi,' rho = ',rho(i)
