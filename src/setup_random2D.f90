8f82557f2437fa903738b2e1d1f7dbee3e4a9f90
Daniel Price <daniel.price@sci.monash.edu.au>
2010-07-16 06:46:04 +0200

    random setup in different file for ASTROSIM

---
+++ src/setup_random2D.f90
@@ -0,0 +1,77 @@
+!----------------------------------------------------------------
+!     Set up a uniform density cartesian grid of particles in ND
+!----------------------------------------------------------------
+
+subroutine setup
+!
+!--include relevant global variables
+!
+ use dimen_mhd
+ use debug
+ use loguns
+ use bound
+ use options
+ use part
+ use setup_params
+ use eos, only:gamma
+ use cons2prim
+ use kernels, only:setkernels,kernelname
+ 
+ use uniform_distributions
+!
+!--define local variables
+!            
+ implicit none
+ integer :: i
+ real :: massp,volume,totmass
+ real :: denszero,rmin,rmax,spsound2
+!
+!--allow for tracing flow
+!
+ if (trace) write(iprint,*) ' entering subroutine setup(unifdis)'
+!
+!--set boundaries
+! 	    
+ ibound = 3     ! boundaries
+ nbpts = 0      ! use ghosts not fixed
+ xmin(:) = 0.   ! set position of boundaries
+ xmax(:) = 1.
+!
+!--set up the uniform density grid
+! 
+ rmin = 0.
+ rmax = 0.5
+
+ call set_uniform_cartesian(4,psep,xmin,xmax,.false.)
+
+ npart = ntotal
+ print*,'npart =',npart
+!
+!--determine particle mass
+!
+ denszero = 1.0
+ volume = product(xmax(:)-xmin(:))
+ totmass = denszero*volume
+ massp = totmass/float(ntotal) ! average particle mass
+ spsound2 = 1.0
+!
+!--now assign particle properties
+! 
+ do i=1,ntotal
+    vel(:,i) = 0.
+    !!!vel(1,i) = x(1,i)
+    dens(i) = denszero
+    pmass(i) = massp
+!    uu(i) = 1.0	! isothermal
+    uu(i) = spsound2/(gamma*(gamma-1.))
+    Bfield(:,i) = 0.
+ enddo
+ 
+ print*,'sound speed = ',sqrt(spsound2), ' sound crossing time = ',(xmax(1)-xmin(1))/sqrt(spsound2)
+!
+!--allow for tracing flow
+!
+ if (trace) write(iprint,*) '  exiting subroutine setup'
+  
+ return
+end
