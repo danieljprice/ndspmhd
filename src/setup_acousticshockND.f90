d16b4f35fd50d1849d740c9f887bf9e5c94c07da
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 01:16:34 +0000

    setup for acoustic-shock problem
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@2635 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/setup_acousticshockND.f90
+++ src/setup_acousticshockND.f90
@@ -27,12 +27,17 @@ subroutine setup
  integer :: i,j
  real :: densleft,densright,prleft,prright
  real :: uuleft, uuright
- real :: dsmooth, exx, delta, const
+ real :: dx, const
  real :: massp,masspleft,masspright,Bxinit,Byleft,Byright,Bzleft,Bzright
  real :: vxleft, vxright, vyleft, vyright, vzleft, vzright
  real, dimension(ndim) :: sidelength,xminleft,xminright,xmaxleft,xmaxright
- real :: boxlength, xshock, gam1, psepleft, psepright
+ real, dimension(ndim) :: xminleftleft,xmaxleftleft
+ real :: boxlength, xshock, gam1, psepleft, psepright, psepleftleft
  real :: total_mass, volume, cs_L,cs2_L,cs2_R,mach_R,mach_L,vjump,gamm1
+ real :: Alayer,mach_ratio,cs
+ real :: densleftleft,vxleftleft,vyleftleft,vzleftleft
+ real :: Bxleftleft,Byleftleft,Bzleftleft
+ real :: vxi,densi,masspleftleft
  character(len=20) :: shkfile
  logical :: equalmass
 !
@@ -44,7 +49,6 @@ subroutine setup
 !
 !--set default values
 !
- dsmooth = 0.   
  equalmass = .true.   ! use equal mass particles??
  const = sqrt(4.*pi)
  gamm1 = gamma - 1.
@@ -62,9 +66,9 @@ subroutine setup
  prright = cs2_R*densright/gamma
  vxright = -sqrt(mach_R**2*cs2_R)
  vxleft = vjump*vxright
- vyleft = 0.01
+ vyleft = 0.0
  vyright = 0.
- vzleft = 0.5
+ vzleft = 0.
  vzright = 0.
  if (imhd.ne.0) then
     Bxinit = 2./const
@@ -72,43 +76,30 @@ subroutine setup
     Byright = 4./const
     Bzleft = 2./const
     Bzright = 2./const
- endif 
+ endif
 !
-!--read shock parameters from the .shk file
+!--now setup parameters specific to the acoustic-shock interaction problem
 !
- shkfile = rootname(1:LEN_TRIM(rootname))//'.shk'
- 
- open(UNIT=ireadf,FILE=shkfile,STATUS='old',FORM='formatted',ERR=666)
-   read(ireadf,*,ERR=667,END=667) densleft,densright
-   read(ireadf,*,ERR=667,END=667) prleft,prright
-   read(ireadf,*,ERR=667,END=667) vxleft,vxright
-   read(ireadf,*,ERR=667,END=667) vyleft,vyright
-   read(ireadf,*,ERR=667,END=667) vzleft,vzright
-   if (imhd.ne.0) then
-      read(ireadf,*,ERR=667,END=667) Bxinit
-      read(ireadf,*,ERR=667,END=667) Byleft,Byright
-      read(ireadf,*,ERR=667,END=667) Bzleft,Bzright
-   endif
- close(UNIT=ireadf)
- goto 668
-666 write(iprint,*) 'shock parameters file not found, using defaults...'
-    write(iprint,*) 'Writing ',shkfile,' with initial left/right states'
-    open(UNIT=ireadf,FILE=shkfile,STATUS='replace',FORM='formatted')
-       write(ireadf,*) densleft,densright
-       write(ireadf,*) prleft,prright
-       write(ireadf,*) vxleft,vxright
-       write(ireadf,*) vyleft,vyright
-       write(ireadf,*) vzleft,vzright
-       write(ireadf,*) Bxinit
-       write(ireadf,*) Byleft,Byright
-       write(ireadf,*) Bzleft,Bzright       
-    close(UNIT=ireadf)
-    
+ xlayer = 0.225
+ dwidthlayer = 1./0.05
+ mach_ratio = 0.5
 
-    goto 668
-667 write(iprint,*) 'error in shock parameters file, using some defaults...'  
-    close(UNIT=ireadf)
-668 continue
+ Alayer = 0.5*(1./gamm1 + 0.5*mach_L**2 &
+       - (1./gamm1 + 0.5*mach_R**2)*(mach_ratio)**(-2.*gamm1/(gamma + 1.)))
+ Alayercs = Alayer*cs_L**2
+!
+!--get density after layer
+!
+ dx = (xmin(1)-xlayer)*dwidthlayer
+ call get_profile(dx,vxi,cs,cs_L,mach_L,Alayer)
+ densleftleft = vxleft*densleft/vxi
+ print*,'Alayer = ',Alayer
+ print*,'mach_L = ',mach_L
+ print*,'machleftleft = ',mach_L*mach_ratio
+ print*,'densleftleft = ',densleftleft
+ print*,'csleftleft = ',cs
+ print*,'vxleftleft = ',vxi
+ print*,'machleftleft = ',vxi/cs
 !
 !--print setup parameters to the log file
 !
@@ -116,7 +107,7 @@ subroutine setup
                   vyleft,vyright,vzleft,vzright
  if (imhd.ne.0) write(iprint,20) Bxinit,Byleft,Byright,Bzleft,Bzright
 
-10 FORMAT(/,1x,i1,'D shock: dens L: ',f8.3,' R: ',f8.3,/,   &
+10 FORMAT(/,1x,i1,'D acoustic-shock: dens L: ',f8.3,' R: ',f8.3,/,   &
            '           pr  L: ',f8.3,' R: ',f8.3,/,   &
            '           vx  L: ',f8.3,' R: ',f8.3,/,   &
            '           vy  L: ',f8.3,' R: ',f8.3,/,   &
@@ -149,11 +140,11 @@ subroutine setup
  if (vxleft.gt.0.) then
     xmin(1) = xmin(1) - vxleft*tmax - 6.*psep
  endif
- if (vxright.lt.0.) then
-    xmax(1) = xmax(1) - vxright*tmax + 6.*psep
- endif
+! if (vxright.lt.0.) then
+!    xmax(1) = xmax(1) - vxright*tmax + 6.*psep
+! endif
  
- xshock = 0.0 !!(xmax(1) + xmin(1))/2.0
+ xshock = 0.8 !!(xmax(1) + xmin(1))/2.0
 
 !
 !--now setup the shock
@@ -163,23 +154,38 @@ subroutine setup
  xmaxleft(:) = xmax(:)
  xminright(:) = xmin(:)
  xmaxright(:) = xmax(:)
+ xminleftleft(:) = xmin(:)
+ xmaxleftleft(:) = xmax(:)
 !
-!--then divide the x axis into two halves at xshock
+!--then divide the x axis into three regions halves at xshock
 !
+ xmaxleftleft(1) = xlayer
+ xminleft(1) = xlayer
  xmaxleft(1) = xshock
  xminright(1) = xshock
+!
+!--particle separation is relative to middle region
+!
  psepleft = psep
  psepright = psep*(densleft/densright)**(1./ndim)
+ psepleftleft = psep*(densleft/densleftleft)**(1./ndim)
 
  if (abs(densleft-densright).gt.1.e-6 .and. equalmass) then
-    print*,' left half  ',xminleft,' to ',xmaxleft,' psepleft = ',psepleft
-    print*,' right half ',xminright,' to ',xmaxright,' psepright = ',psepright
+    print*,' left region  ',xminleftleft,' to ',xmaxleftleft,' psepleft = ',psepleftleft
+    print*,' middle region  ',xminleft,' to ',xmaxleft,' psepleft = ',psepleft
+    print*,' right region ',xminright,' to ',xmaxright,' psepright = ',psepright
 !!    massp = (psep**ndim)*densright
+
+    call set_uniform_cartesian(2,psepleftleft,xminleftleft,xmaxleftleft,.false.)  ! set left half
+    xmin = xminleftleft
+!
+!--particle volume is relative to middle region
+!
     call set_uniform_cartesian(2,psepleft,xminleft,xmaxleft,.false.)  ! set left half
-    xmin = xminleft
     volume = PRODUCT(xmaxleft-xminleft)
     total_mass = volume*densleft
     massp = total_mass/npart
+    masspleftleft = massp
     masspleft = massp
     masspright = massp
  
@@ -192,6 +198,7 @@ subroutine setup
     masspleft = densleft*volume/REAL(npart)
 !    vol_right = PRODUCT(xmaxright-xminright)
     masspright = densright*volume/REAL(npart)
+    masspleftleft = densleftleft*volume/REAL(npart)
  endif
 
  print*,'npart = ',npart
@@ -221,8 +228,7 @@ subroutine setup
  endif
 
  do i=1,npart
-    delta = (x(1,i) - xshock)/psep
-    if (delta.GT.dsmooth) then
+    if (x(1,i).GT.xminright(1)) then
        dens(i) = densright
        uu(i) = uuright 
        vel(1,i) = vxright
@@ -231,7 +237,7 @@ subroutine setup
        if (ndimV.ge.3) vel(3,i) = vzright 
        if (ndimV.ge.2) Bfield(2,i) = Byright
        if (ndimV.ge.3) Bfield(3,i) = Bzright
-    elseif (delta.LT.-dsmooth) then
+    elseif (x(1,i).gt.xminleft(1)) then
        dens(i) = densleft
        pmass(i) = masspleft
        uu(i) = uuleft
@@ -240,26 +246,18 @@ subroutine setup
        if (ndimV.ge.3) vel(3,i) = vzleft
        if (ndimV.ge.2) Bfield(2,i) = Byleft
        if (ndimV.ge.3) Bfield(3,i) = Bzleft
-    else
-       exx = exp(delta)       
-       dens(i) = (densleft + densright*exx)/(1.0 +exx)
-       pmass(i) = (masspleft + masspright*exx)/(1.0 + exx)
-!       uu(i) = (uuleft + uuright*exx)/(1.0 + exx)
-       uu(i) = (prleft + prright*exx)/((1.0 + exx)*gam1*dens(i))
-!       vel(1,i) = (vxleft + vxright*exx)/(1.0 + exx)
-!       if (ndimV.ge.2) vel(2,i) = (vyleft + vyright*exx)/(1.0 + exx)
-!       if (ndimV.ge.3) vel(3,i) = (vzleft + vzright*exx)/(1.0 + exx)
-       if (delta.GT.0.) THEN
-          vel(1,i) = vxright
-          if (ndimV.ge.2) vel(2,i) = vyright
-          if (ndimV.ge.3) vel(3,i) = vzright 
-       else
-          vel(1,i) = vxleft
-          if (ndimV.ge.2) vel(2,i) = vyleft
-          if (ndimV.ge.3) vel(3,i) = vzleft       
-       endif
-       if (ndimV.ge.2) Bfield(2,i) = (Byleft + Byright*exx)/(1.0 + exx)
-       if (ndimV.ge.3) Bfield(3,i) = (Bzleft + Bzright*exx)/(1.0 + exx)      
+    elseif (x(1,i).gt.xminleftleft(1)) then
+       dx = (x(1,i) - xlayer)*dwidthlayer
+       call get_profile(dx,vxi,cs,cs_L,mach_L,Alayer)
+       densi = vxleft*densleft/vxi
+       dens(i) = densi
+       pmass(i) = masspleftleft
+       uu(i) = cs**2/(gamma*gamm1)
+       vel(1,i) = vxleftleft
+       if (ndimV.ge.2) vel(2,i) = vyleftleft
+       if (ndimV.ge.3) vel(3,i) = vzleftleft
+       if (ndimV.ge.2) Bfield(2,i) = Byleftleft
+       if (ndimV.ge.3) Bfield(3,i) = Bzleftleft    
     endif           
     Bfield(1,i) = Bxinit
  enddo
@@ -270,4 +268,44 @@ subroutine setup
  Bconst(1) = Bxinit
 
  return
+ 
+contains
+
+subroutine get_profile(dx,velx,cs,cs_L,mach_L,Alayer)
+ implicit none
+ real, intent(in) :: dx,mach_L,cs_L,Alayer
+ real, intent(out) :: velx,cs
+ integer, parameter :: maxits = 1000
+ integer :: its
+ real, parameter :: tol = 1.e-10
+ real :: machno,machmin,machmax,test
+ 
+ machmin = 0.
+ machmax = 1.0
+ its = 0
+!
+!--iterate using bisection method
+!
+ do while (abs(1.-machmin/machmax).gt.tol .and. its.le.maxits)
+    machno = 0.5*(machmin + machmax)
+    test = (machno/mach_L)**(2.*(gamma-1.)/(gamma+1.))* &
+           (Alayer*(TANH(dx)-1.) + 1./(gamma-1.) + 0.5*mach_L**2) &
+           -0.5*machno**2 - 1./(gamma-1.)
+    if (test.lt.0.) then
+       machmin = machno
+    else
+       machmax = machno
+    endif
+    its = its + 1
+ enddo
+ 
+ if (its.eq.maxits) STOP 'ERROR: mach number not converged in setup'
+ 
+ print*,'machno = ',machno
+ machno = 0.5*(machmin + machmax)
+ cs = cs_L*(mach_L/machno)**((gamma-1.)/(gamma + 1.))
+ velx = -machno*cs
+ 
+end subroutine get_profile
+
 end subroutine setup
