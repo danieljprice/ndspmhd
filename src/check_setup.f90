5b735ea0fc2e082191770e120f5f26d97b1f2b8b
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 01:13:05 +0000

    huge value checks
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@2550 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/check_setup.f90
+++ src/check_setup.f90
@@ -2,13 +2,16 @@
 !--checks (user defined) setup of primitive quantities for errors
 !
 subroutine check_setup
+  use dimen_mhd, only:ndim
   use debug
   use loguns
   use part
   implicit none
   integer :: i
+  real, parameter :: vbig = 1.e12
+  real, dimension(ndim) :: xcentre
   
-  write(iprint,5,ADVANCE='NO') ' Checking setup... '
+  write(iprint,5) ' Checking setup... '
 5 format(/,a)
 
   if (npart.le.0) then
@@ -20,28 +23,45 @@ subroutine check_setup
      stop
   endif
 
+  xcentre(:) = 0.
   do i=1,npart
 !
 !--check for negative densities/thermal energies
 !
-     if (dens(i).le.0.) then
+     if (dens(i).lt.tiny(dens)) then
         write(iprint,20) 'density <= 0 ',dens(i),i
         stop
      endif
-     if (pmass(i).le.0.) then
+     if (pmass(i).lt.tiny(pmass)) then
         write(iprint,20) 'pmass <= 0 ',pmass(i),i
         stop
+     elseif (pmass(i).gt.vbig) then
+        write(iprint,20) 'huge pmass values ',pmass(i),i
+        stop
      endif     
-     if (uu(i).lt.0.) then
+     if (uu(i).lt.tiny(uu)) then
         write(iprint,20) 'uu < 0 ',uu(i),i
         stop
      endif
+!
+!--check for huge velocities
+!
+     if (any(abs(vel(1:ndimV,i)).gt.vbig)) then
+        write(iprint,10) ' contains huge velocities!! '
+        stop
+     endif
+     
+     xcentre(:) = xcentre(:) + pmass(i)*x(:,i)
      
   enddo
+  
+  xcentre = xcentre / SUM(pmass(1:npart))
 
 10 format(/,' ERROR IN PARTICLE SETUP: ',a,/)
 20 format(/,' ERROR IN PARTICLE SETUP: ',a,1pe10.3,' particle ',i5,/)
 
-   write(iprint,*) 'OK'
+   write(iprint,*) '-> centre of mass is at : ',xcentre(:)
+
+   write(iprint,*) '-> OK'
 
 end subroutine check_setup
