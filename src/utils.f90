2e9b9e8fde421ff36dfad4e40a54cf0fac7b22fe
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 02:34:59 +0000

    added curl3D_epsijk utility for getting curl from grad of a vector
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@4316 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/utils.f90
+++ src/utils.f90
@@ -36,3 +36,14 @@ subroutine cross_product3D(veca,vecb,vecc)
  vecc(3) = veca(1)*vecb(2) - veca(2)*vecb(1)
 
 end subroutine cross_product3D
+
+subroutine curl3D_epsijk(gradAvec,curlA)
+ implicit none
+ real, dimension(3,3), intent(in) :: gradAvec
+ real, dimension(3), intent(out) :: curlA
+ 
+ curlA(1) = gradAvec(2,3) - gradAvec(3,2)
+ curlA(2) = gradAvec(3,1) - gradAvec(1,3)
+ curlA(3) = gradAvec(1,2) - gradAvec(2,1)
+
+end subroutine curl3D_epsijk
