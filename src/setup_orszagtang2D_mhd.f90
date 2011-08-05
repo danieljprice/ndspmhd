a6f12b358f1c698f09d2eb2abde95d9d8b45b91d
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 02:16:17 +0000

    symmetric orszag-tang option implemented; also uses xmin explicitly
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@3851 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/setup_orszagtang2D_mhd.f90
+++ src/setup_orszagtang2D_mhd.f90
@@ -19,15 +19,18 @@ subroutine setup
  use options
  use part
  use setup_params
+ use mem_allocation, only:alloc
  
  use uniform_distributions
 !
 !--define local variables
 !      
  implicit none
- integer :: ipart
+ integer :: ipart,i
  real :: betazero,denszero,przero,vzero,bzero,uuzero,machzero
  real :: totmass,gam1,massp,const
+ real, dimension(ndim) :: xminregion, xmaxregion
+ logical, parameter :: symmetric = .true.
 !
 !--check number of dimensions is right
 !
@@ -37,10 +40,17 @@ subroutine setup
 !                        
  ibound = 3     ! periodic
  nbpts = 0      ! must use fixed particles if inflow/outflow at boundaries
- xmin(1) = 0.0  ! x
- xmax(1) = 1.0
- xmin(2) = 0.0  ! y
- xmax(2) = 1.0
+ if (symmetric) then
+    xmin(1) = -0.5  ! x
+    xmax(1) = -xmin(1)
+    xmin(2) = -0.5  ! y
+    xmax(2) = -xmin(2)
+ else
+    xmin(1) = 0.  ! x
+    xmax(1) = 1.0
+    xmin(2) = 0.0  ! y
+    xmax(2) = 1.0
+ endif
 !
 !--setup parameters
 !
@@ -59,11 +69,67 @@ subroutine setup
  write(iprint,10) betazero,machzero,bzero,denszero,przero
 10 format(/,' beta        = ',f6.3,', mach number = ',f6.3,/, &
             ' initial B   = ',f6.3,', density = ',f6.3,', pressure = ',f6.3,/)
+
+ if (symmetric) then
+    xminregion(1) = xmin(1)
+    xmaxregion(1) = 0.5*(xmax(1)+xmin(1))
+    xminregion(2) = xmin(2)
+    xmaxregion(2) = 0.5*(xmax(2)+xmin(2))
+    call set_uniform_cartesian(1,psep,xminregion,xmaxregion,fill=.true.)
+
+    do ipart=1,npart
+       vel(1,ipart) = -vzero*sin(2.*pi*(x(2,ipart)-xmin(2)))
+       vel(2,ipart) = vzero*sin(2.*pi*(x(1,ipart)-xmin(1)))
+       if (imhd.lt.0) then
+          Bevol(3,ipart) = 0.5/pi*Bzero*(cos(2.*pi*(x(2,ipart)-xmin(2))) + 0.5*cos(4.*pi*(x(1,ipart)-xmin(1))))
+       endif
+    enddo
+!
+!--reallocate memory to new size of list
+!
+    call alloc(4*npart)
+!
+!--reflect particles across x=0 axis
+!
+    print*,'reflecting across x'
+    ipart = npart
+    do i=1,npart
+       ipart = ipart + 1
+       x(1,ipart) = -x(1,i)
+       x(2,ipart) = x(2,i)
+       vel(1,ipart) = vel(1,i)
+       vel(2,ipart) = -vel(2,i)
+       if (imhd.lt.0) then
+          Bevol(3,ipart) = Bevol(3,i)
+       endif
+    enddo
+    npart = ipart
+!
+!--reflect particles across y=0 axis
+!
+    print*,'reflecting across y'
+    ipart = npart
+    do i=1,npart
+       ipart = ipart + 1
+       x(1,ipart) = x(1,i)
+       x(2,ipart) = -x(2,i)
+       vel(1,ipart) = -vel(1,i)
+       vel(2,ipart) = vel(2,i)
+       if (imhd.lt.0) then
+          Bevol(3,ipart) = Bevol(3,i)
+       endif
+    enddo
+    npart = ipart
+    ntotal = npart
+
+ else
 !
 !--setup uniform density grid of particles (2D) with sinusoidal field/velocity
 !  determines particle number and allocates memory
 !
- call set_uniform_cartesian(2,psep,xmin,xmax)        ! 2 = close packed arrangement
+    call set_uniform_cartesian(2,psep,xmin,xmax)        ! 2 = close packed arrangement
+
+ endif
 
  ntotal = npart
 !
@@ -75,21 +141,25 @@ subroutine setup
 !--now assign particle properties
 ! 
  do ipart=1,ntotal
-    vel(1,ipart) = -vzero*sin(2.*pi*x(2,ipart))
-    vel(2,ipart) = vzero*sin(2.*pi*x(1,ipart))
+    if (.not.symmetric) then
+       vel(1,ipart) = -vzero*sin(2.*pi*(x(2,ipart)-xmin(2)))
+       vel(2,ipart) = vzero*sin(2.*pi*(x(1,ipart)-xmin(1)))
+    endif
     if (ndimv.eq.3) vel(3,ipart) = 0.
     dens(ipart) = denszero
     pmass(ipart) = massp
     uu(ipart) = uuzero
     if (imhd.ge.1) then
-       Bfield(1,ipart) = -Bzero*sin(2.*pi*x(2,ipart))
-       Bfield(2,ipart) = Bzero*sin(4.*pi*x(1,ipart))
+       Bfield(1,ipart) = -Bzero*sin(2.*pi*(x(2,ipart)-xmin(2)))
+       Bfield(2,ipart) = Bzero*sin(4.*pi*(x(1,ipart)-xmin(1)))
        if (ndimv.eq.3) Bfield(3,ipart) = 0.0
     elseif (imhd.lt.0) then
 !--vector potential setup
-       Bevol(:,ipart) = 0.
        if (ndimV.lt.3) stop 'ndimV too small in setup_orstang'
-       Bevol(3,ipart) = 0.5/pi*Bzero*(cos(2.*pi*x(2,ipart)) + 0.5*cos(4.*pi*x(1,ipart)))
+       if (.not.symmetric) then
+          Bevol(:,ipart) = 0.
+          Bevol(3,ipart) = 0.5/pi*Bzero*(cos(2.*pi*(x(2,ipart)-xmin(2))) + 0.5*cos(4.*pi*(x(1,ipart)-xmin(1))))
+       endif
     else
        Bfield(:,ipart) = 0.
     endif 
