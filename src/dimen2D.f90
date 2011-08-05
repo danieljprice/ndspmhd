bd53c72d1d784ccd371529d79c12e4ef176b7680
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 00:31:57 +0000

    25D options removed - 2D now always 2.5D
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@2073 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/dimen2D.f90
+++ src/dimen2D.f90
@@ -6,7 +6,7 @@
 MODULE dimen_mhd
  IMPLICIT NONE
  INTEGER, PARAMETER :: ndim = 2		! Dimensions
- INTEGER, PARAMETER :: ndimB = 2	! Dimensions of Magnetic field variable
+ INTEGER, PARAMETER :: ndimB = 3	! Dimensions of Magnetic field variable
  INTEGER, PARAMETER :: ndimV = ndimB	! Dimensions of Velocity variable
  REAL :: dndim	! for divisions by ndim (set in initialise)
 END MODULE
