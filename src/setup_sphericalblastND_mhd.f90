adcfbc8ae9102e0087a3f328f965481c14aac57c
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 01:59:27 +0000

    allows for relaxation run beforehand; also energy bug fix
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@3415 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/setup_sphericalblastND_mhd.f90
+++ src/setup_sphericalblastND_mhd.f90
@@ -21,9 +21,9 @@ subroutine setup
  use bound
  use eos
  use kernels, only:interpolate_kernel
- use options
+ use options, only:damp,ibound
  use part
- use setup_params
+ use setup_params, only:psep
  
  use uniform_distributions
 !
@@ -32,12 +32,8 @@ subroutine setup
  implicit none
  integer :: ipart
  real :: denszero,przero
- real :: prblast,pri,uui,rblast,radius,enblast,enzero
- real :: totmass,gam1,massp,const
- real, dimension(ndim) :: xblast, dblast
- real, dimension(ndimV) :: Bzero
- real :: rbuffer, exx, hsmooth
- real :: q2, wab, grkern
+ real :: totmass,gam1,massp
+
 !
 !--set boundaries
 !            	    
@@ -45,41 +41,19 @@ subroutine setup
  nbpts = 0
  xmin(:) = -0.5		! same xmin in all dimensions
  xmax(:) = 0.5
- xmin(2) = -0.75
- xmax(2) = 0.75
- const = 1./sqrt(4.*pi) 
-!
-!--setup parameters for the problem
-! 
- xblast(:) = 0.0	! co-ordinates of the centre of the initial blast
- rblast = 0.0     !!!05    !!!*psep		! radius of the initial blast
- rbuffer = rblast	!+10.*psep		! radius of the smoothed front
- bzero(:) = 0.0
- if (imhd.ne.0) then
-    bzero(1) = sqrt(2.*pi)         !10.0*const	! uniform field in bx direction
-    bzero(2) = sqrt(2.*pi)
- endif
- przero = 0.1		! initial pressure
+! xmin(2) = -0.75
+! xmax(2) = 0.75
  denszero = 1.0
- prblast = 10.0	! initial pressure within rblast
- enblast = 1.0
- enzero = 0.
+ przero = 0.1
  
  gam1 = gamma - 1.
  if (abs(gam1).lt.1.e-3) stop 'eos cannot be isothermal for this setup'
 
- write(iprint,10) ndim
- write(iprint,20) prblast,rblast,denszero,przero
- write(iprint,30) Bzero
-10 format(/,1x,i1,'-dimensional adiabatic mhd blast wave problem')
-20 format(/,' central pressure  = ',f10.3,', blast radius = ',f6.3,/, &
-            ' density = ',f6.3,', pressure = ',f6.3,/)
-30 format(' initial B   = ',3(f6.3,1x))
 !
 !--setup uniform density grid of particles
 !  (determines particle number and allocates memory)
 !
- call set_uniform_cartesian(1,psep,xmin,xmax,offset=.true.,perturb=0.5)	! 2 = close packed arrangement
+ call set_uniform_cartesian(1,psep,xmin,xmax,offset=.true.)	! 2 = close packed arrangement
 
  ntotal = npart
 !
@@ -87,13 +61,6 @@ subroutine setup
 !
  totmass = denszero*product(xmax(:)-xmin(:))	! assumes cartesian boundaries
  massp = totmass/float(ntotal) ! average particle mass
- !enblast = enblast/massp   ! enblast is now the energy to put in a single particle
- !enblast = enblast/(4./3.*pi*rblast**3)
-!
-!--smoothing length for kernel smoothing
-! 
- hsmooth = hfact*(massp/denszero)**dndim
- write(iprint,*) 'hsmooth = ',hsmooth
 !
 !--now assign particle properties
 ! 
@@ -102,33 +69,12 @@ subroutine setup
 !--uniform density and smoothing length
     dens(ipart) = denszero
     pmass(ipart) = massp
-
-    dblast(:) = x(:,ipart)-xblast(:) 
-    radius = sqrt(dot_product(dblast,dblast))
-!
-!--smooth energy injection using the sph kernel
-!    
-!    q2 = radius**2/hsmooth**2
-!    call interpolate_kernel(q2,wab,grkern)
-!    uui = enblast*wab/hsmooth**ndim
-    if (radius.lt.rblast) then
-       pri = prblast
-       !uui = enblast
-    elseif (radius.lt.rbuffer) then	! smooth out front
-       exx = exp((radius-rblast)/(psep))
-       pri = (prblast + przero*exx)/(1.0+exx)
-       !uui = (enblast + enzero*exx)/(1.0+exx)
-    else
-       pri = przero
-       !uui = enzero
-    endif   
-    uu(ipart) = pri/(gam1*denszero)
-    bfield(:,ipart) = bzero(:)
+    uu(ipart) = przero/(gam1*denszero)
  enddo
 !
-!--set the constant components of the mag field which can be subtracted
+!--add the blast wave if damping is off
 !
- bconst(:) = bzero(:)
+ if (abs(damp).lt.tiny(damp)) call modify_dump
 
 !
 !--allow for tracing flow
@@ -141,28 +87,61 @@ end
 subroutine modify_dump
  use loguns, only:iprint
  use part
+ use options, only:imhd
  use timestep, only:time
- use setup_params, only:pi
+ use setup_params, only:pi,psep,hfact
  use eos, only:gamma
  implicit none
  integer :: ipart
- real :: rblast,radius,dblast(ndim),przero,prblast,pri,gam1,denszero
-!
-!--now assign particle properties
-!
- write(iprint,*) 'modifying dump with velocities/B field'
-!
-!--now assign particle properties
-! 
- rblast = 0.1
- prblast = 10.0
- przero = 0.1
+ real :: prblast,pri,uui,rblast,radius,enblast,enzero,const
+ real :: rblast,radius,dblast(ndim),przero,gam1,denszero
+ real, dimension(ndim) :: xblast, dblast
+ real, dimension(ndimV) :: Bzero
+ real :: rbuffer, exx, hsmooth
+ real :: q2, wab, grkern
+ 
+ write(iprint,*) 'modifying dump by adding blast'
+
  gam1 = gamma - 1.0
+ if (abs(gam1).lt.1.e-3) stop 'eos cannot be isothermal for this setup'
+!
+!--setup parameters for the problem
+! 
+ xblast(:) = 0.0	! co-ordinates of the centre of the initial blast
+ rblast = 2.*hfact*psep		! radius of the initial blast
+ rbuffer = rblast	!+10.*psep		! radius of the smoothed front
+ bzero(:) = 0.0
+ const = 1./sqrt(4.*pi) 
+ if (imhd.ne.0) then
+    bzero(1) = sqrt(2.*pi)         !10.0*const	! uniform field in bx direction
+    bzero(2) = sqrt(2.*pi)
+ endif
+ przero = 0.0		! initial pressure
  denszero = 1.0
+ prblast = 10.0	! initial pressure within rblast
+ enblast = 1.0
+ enzero = 0.
+ prblast = gam1*enblast/(4./3.*pi*rblast**3)
+ !enblast = enblast/massp   ! enblast is now the energy to put in a single particle
+!
+!--smoothing length for kernel smoothing
+! 
+ hsmooth = hfact*(pmass(1)/denszero)**dndim
+! write(iprint,*) 'hsmooth = ',hsmooth
+
+ write(iprint,10) ndim
+ write(iprint,20) prblast,rblast,denszero,przero
+10 format(/,1x,i1,'-dimensional adiabatic mhd blast wave problem')
+20 format(/,' central pressure  = ',f10.3,', blast radius = ',f6.3,/, &
+            ' density = ',f6.3,', pressure = ',f6.3,/)
+
+
  do ipart=1,ntotal
     vel(:,ipart) = 0.
+!--uniform density and smoothing length
+    dens(ipart) = denszero
 
-    dblast(:) = x(:,ipart)
+    dblast(:) = x(:,ipart)-xblast(:) 
     radius = sqrt(dot_product(dblast,dblast))
 !
 !--smooth energy injection using the sph kernel
@@ -170,15 +149,26 @@ subroutine modify_dump
 !    q2 = radius**2/hsmooth**2
 !    call interpolate_kernel(q2,wab,grkern)
 !    uui = enblast*wab/hsmooth**ndim
-    if (radius.lt.rblast) then
+    if (radius.le.rblast) then
        pri = prblast
        !uui = enblast
+    elseif (radius.lt.rbuffer) then	! smooth out front
+       exx = exp((radius-rblast)/(psep))
+       pri = (prblast + przero*exx)/(1.0+exx)
+      !uui = (enblast + enzero*exx)/(1.0+exx)
     else
        pri = przero
        !uui = enzero
     endif   
     uu(ipart) = pri/(gam1*denszero)
+    bfield(:,ipart) = bzero(:)
  enddo
+ 
+!
+!--set the constant components of the mag field which can be subtracted
+!
+ bconst(:) = bzero(:)
+ 
  time = 0.
  
 end subroutine modify_dump
