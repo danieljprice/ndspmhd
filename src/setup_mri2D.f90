920656335ac66eae900804bc056bfbadc0683226
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 02:19:05 +0000

    various changes
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@3913 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/setup_mri2D.f90
+++ src/setup_mri2D.f90
@@ -22,10 +22,11 @@ subroutine setup
 !--define local variables
 !            
  implicit none
- integer :: i,j,ipart,npartr,npartz
- real :: massp,volume,totmass,deltar,deltaz,deltarav,rpos,zpos
+ integer :: i,iseed
+ real :: massp,totmass
  real :: denszero,uuzero,cs0,polyk0
- real :: przero,Bzeroz,asize,betamhd
+ real :: przero,Bzeroz,asize,betamhd,ran1,wavekmin,valfvenz
+ integer, parameter :: ipert = 1
 !
 !--allow for tracing flow
 !
@@ -45,9 +46,9 @@ subroutine setup
 !
 !--set boundaries
 !
- ibound(:) = 3	! periodic in y,z
+ ibound(:) = 3  ! periodic in y,z
  ibound(1) = 5  ! shearing box in x (==R)  
- nbpts = 0	! use ghosts not fixed
+ nbpts = 0      ! use ghosts not fixed
  asize = 1.0    ! box size (corresponds to a in Hawley/Balbus 1992)
  xmin(:) = -0.5*asize  ! set position of boundaries
  xmax(:) = 0.5*asize
@@ -68,31 +69,64 @@ subroutine setup
 !--sound speed
 !
  przero = 1.e-5
- cs0 = gamma*przero/denszero
+ cs0 = sqrt(gamma*przero/denszero)
  uuzero = przero/(denszero*(gamma-1.))
  polyk0 = przero/denszero**gamma
- write(iprint,*) ' cs0 = ',cs0, ' u = ',uuzero
- write(iprint,*) ' polyk = ',polyk,' should be = ',polyk0
+ write(iprint,*) ' cs0 = ',cs0,' cs2 = ',cs0**2,' u = ',uuzero
+ write(iprint,*) ' polyk = ',polyk,' setting to = ',polyk0
  polyk = polyk0
 
  write(iprint,*) ' Omega_c = ',Omega0, ' R_c = ',Rcentre
+ write(iprint,*) ' orbital time = ',2.*pi/Omega0
+ write(iprint,*) ' c / (R omega) = ',cs0/(Rcentre*Omega0)
 
 !
 !--field strength (beta = pr/(0.5*B^2))
 !
- betamhd = 400.
+ betamhd = 4000.
  Bzeroz = sqrt(2.*przero/betamhd)
  write(iprint,*) ' beta(MHD) = ',betamhd,' Bz = ',Bzeroz,' P = ',przero
+
+!--
+ valfvenz = sqrt(Bzeroz**2/denszero)
+ wavekmin = sqrt(15.)/4.*Omega0/valfvenz
+ 
+! wavekmin = 2.*pi/((xmax(2)-xmin(2))*Omega0)*sqrt(2.*przero/(denszero*betamhd))
+! write(iprint,*) ' qmin = ',wavekmin
+ write(iprint,*) 'most unstable wavelength = ',2.*pi/wavekmin
+
 !
 !--now assign particle properties
 ! 
+ iseed = -213
  do i=1,ntotal
-    vel(:,i) = 0.
-    vel(2,i) = -domegadr*Omega0*x(1,i)
+    vel(:,i) = 0. !0.01*(ran1(iseed)-0.5)*cs0
+    vel(3,i) = -domegadr*Omega0*x(1,i))
     dens(i) = denszero
     pmass(i) = massp
-    uu(i) = uuzero ! isothermal
-    Bfield(:,i) = 0.
+    uu(i) = uuzero   !*(1. + 0.01*(ran1(iseed)-0.5)) ! isothermal
+    select case(ipert)
+    case(2)
+       vel(1,i) = 0.
+    case(1)
+       vel(1,i) = 0.01*cs0
+    case default
+       stop 'invalid ipert'
+    end select
+    
+    if (imhd.gt.0) then
+       Bfield(:,i) = 0.
+       Bfield(2,i) = Bzeroz !*sin(2.*pi*x(1,i))
+       !if (abs(x(1,i)).lt.0.25*asize) then
+       !   Bfield(2,i) = Bzeroz !*(1. + 0.01*sin(4.*2.*pi*(x(2,i)-xmin(2))))
+       !else
+       !   Bfield(2,i) = 0.
+       !endif
+       Bfield(3,i) = 0.
+    elseif (imhd.lt.0) then
+       Bevol(:,i) = 0.
+       Bevol(3,i) = 0. !0.5/pi*Bzeroz*cos(2.*pi*x(1,i))
+    endif
  enddo 
 !
 !--allow for tracing flow
