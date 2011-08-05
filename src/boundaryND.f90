089e93db15bf766730f82e33a60c517e742fb82f
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 01:54:26 +0000

    boundary adjustments implemented
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@3270 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/boundaryND.f90
+++ src/boundaryND.f90
@@ -5,61 +5,87 @@
 !! for ND case only periodic boundaries implemented
 !!-------------------------------------------------------------------------
 	 
-SUBROUTINE boundary
- USE dimen_mhd
- USE debug
- USE loguns
+subroutine boundary
+ use dimen_mhd, only:ndim
+ use debug, only:trace
+ use loguns, only:iprint
  
- USE bound
- USE options
- USE part
- USE part_in
-! USE setup_params
+ use bound, only:xmin,xmax
+ use options, only:ibound
+ use part, only:x,vel,npart
+! use rates, only:force
+! use part_in
+! use setup_params
 !
 !--define local variables
 !
- IMPLICIT NONE
- INTEGER :: i,j,nnew,nsub,jdim
- REAL :: psepleft,psepright
- LOGICAL :: debugging
+ implicit none
+ integer :: i,jdim,ncorrect
+ logical :: debugging
 !
 !--allow for tracing flow
 !      
- IF (trace) THEN
-    WRITE(iprint,*) ' Entering subroutine boundary'
+ if (trace) then
+    write(iprint,*) ' entering subroutine boundary'
     debugging = .true.
- ENDIF   
+ endif   
  
 !---------------------------------------------------------------------------
 !  inflow/outflow when fixed particles are used.
 !---------------------------------------------------------------------------     
- IF (ANY(ibound.EQ.1)) THEN
-    IF (ndim.GT.1) WRITE(iprint,*) ' inflow/outflow not implemented in ND'
- ENDIF
+ if (any(ibound.eq.1)) then
+    if (ndim.gt.1) write(iprint,*) ' inflow/outflow not implemented in nd'
+ endif
 !----------------------------------------------------------------------
 ! periodic boundary conditions - allow particles to cross the domain
 !----------------------------------------------------------------------
-
- IF (ANY(ibound.EQ.3)) THEN
-    
-    DO i=1,npart
-
-       DO jdim=1,ndim
-          IF (ibound(jdim).EQ.3) THEN
-	     IF (x(jdim,i).GT.xmax(jdim)) THEN
+ if (any(ibound.eq.3)) then   
+    do i=1,npart
+       do jdim=1,ndim
+          if (ibound(jdim).eq.3) then
+	     if (x(jdim,i).gt.xmax(jdim)) then
 !	        print*,'ss xold,xmax,xnew = ',jdim,x(jdim,i),xmax(jdim),xmin(jdim) + x(jdim,i) - xmax(jdim)
 	        x(jdim,i) = xmin(jdim) + x(jdim,i) - xmax(jdim)
-	     ELSEIF(x(jdim,i).LT.xmin(jdim)) THEN
+	     elseif(x(jdim,i).lt.xmin(jdim)) then
 !	        print*,'ss xold,xmin,xnew = ',jdim,x(jdim,i),xmin(jdim),xmax(jdim) + x(jdim,i) - xmin(jdim)	     
 !	        read*
                 x(jdim,i) = xmax(jdim) - (xmin(jdim) - x(jdim,i))
-             ENDIF	  
-	  ENDIF
-       ENDDO
-    
-    ENDDO   
+             endif	  
+	  endif
+       enddo    
+    enddo   
+ elseif (any(ibound.gt.1)) then
+!-------------------------------------------------------------------------
+! other ghost boundaries - correct particles which have crossed boundary
+!-------------------------------------------------------------------------
+    ncorrect = 0
+    do i=1,npart
+       do jdim=1,ndim
+          if (ibound(jdim).gt.1) then
+             if (x(jdim,i).gt.xmax(jdim)) then
+                !--move particle back inside boundary
+                x(jdim,i) = xmax(jdim) - (x(jdim,i) - xmax(jdim))
+                !--flip normal velocity component
+                vel(jdim,i) = -vel(jdim,i)
+!                force(jdim,i) = -force(jdim,i)
+                ncorrect = ncorrect + 1
+!                print*,'xmax',jdim,i,'xnew = ',x(jdim,i),vel(jdim,i)
+	     elseif(x(jdim,i).lt.xmin(jdim)) then
+!                print*,'xminold',jdim,i,'x,v = ',x(jdim,i),vel(jdim,i)
+                !--move particle back inside boundary
+                x(jdim,i) = xmin(jdim) + (xmin(jdim) - x(jdim,i))
+                !--flip normal velocity component
+                vel(jdim,i) = -vel(jdim,i)
+!                force(jdim,i) = -force(jdim,i)
+                ncorrect = ncorrect + 1
+!                print*,'xminnew',jdim,i,'x,v = ',x(jdim,i),vel(jdim,i)
+             endif
+          endif
+       enddo
+    enddo
+    if (ncorrect.gt.0) write(iprint,*) ' warning: ',ncorrect,' particles '// &
+                                       ' adjusted from boundary'
+ endif
 
- ENDIF
-
- RETURN 
-END SUBROUTINE boundary
+ return 
+end subroutine boundary
