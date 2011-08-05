79d6e0997686899755062d62413bac25f62d6ae9
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 00:22:37 +0000

    restored flush commands
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@1878 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/outputND_mhd.f90
+++ src/outputND_mhd.f90
@@ -23,7 +23,7 @@ SUBROUTINE output(t,nstep)
  INTEGER, INTENT(IN) :: nstep
  INTEGER :: nprint,ndata
  INTEGER :: i,ierr(3),istat
-!! INTEGER, EXTERNAL :: flush
+ INTEGER, EXTERNAL :: flush
 !
 !--allow for tracing flow
 !      
@@ -78,10 +78,10 @@ SUBROUTINE output(t,nstep)
 !
 !--flush the buffer so that whole timestep is printed even if program crashes
 ! 
-!! ierr(1) = flush(idatfile)
-!! ierr(2) = flush(ievfile)
-!! ierr(3) = flush(iprint)
-!! IF ( ANY(ierr.NE.0) ) WRITE(*,*) 'Error flushing files, see Makefile ',ierr 
+ ierr(1) = flush(idatfile)
+ ierr(2) = flush(ievfile)
+ ierr(3) = flush(iprint)
+ IF ( ANY(ierr.NE.0) ) WRITE(*,*) 'Error flushing files, see Makefile ',ierr 
   			  
 30    FORMAT (23(1pe14.6,1x),:)	! make sure the format statement has >/=	
 				! max number of columns in the write statement
