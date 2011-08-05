109f66a159f8dbdf30f21afe33a28177e086e826
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 02:35:16 +0000

    uses utils module; lower case
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@4324 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/write_headerND_mhd.f90
+++ src/write_headerND_mhd.f90
@@ -6,57 +6,58 @@
 !! icall = 2 (after particle setup)
 !!-----------------------------------------------------------------
 
-SUBROUTINE write_header(icall,infile,evfile,logfile)
- USE dimen_mhd
- USE debug
- USE loguns
+subroutine write_header(icall,infile,evfile,logfile)
+ use dimen_mhd
+ use debug
+ use loguns
  
- USE kernels, only:ianticlump,eps,neps,radkern
- USE artvi
- USE bound
- USE hterms, only:rhomin
- USE eos
- USE options
- USE setup_params
- USE part
- USE timestep
- USE versn
- USE xsph
+ use kernels, only:ianticlump,eps,neps,radkern
+ use artvi
+ use bound
+ use hterms, only:rhomin
+ use eos
+ use options
+ use setup_params
+ use part
+ use timestep
+ use versn
+ use xsph
+ use utils, only:minmaxave
 !
 !--define local variables
 !
- IMPLICIT NONE
- INTEGER :: i
- INTEGER, INTENT(IN) :: icall
- CHARACTER(LEN=*), INTENT(IN) :: infile,evfile,logfile
- CHARACTER(LEN=21) :: boundtype
- CHARACTER(LEN=5), DIMENSION(3) :: coord
- CHARACTER(LEN=10) :: startdate, starttime
- REAL :: have,hmin,hmax,v2i,B2i,fNneigh
- REAL, DIMENSION(npart) :: dummy
+ implicit none
+ integer :: i
+ integer, intent(in) :: icall
+ character(len=*), intent(in) :: infile,evfile,logfile
+ character(len=21) :: boundtype
+ character(len=5), dimension(3) :: coord
+ character(len=10) :: startdate, starttime
+ real :: have,hmin,hmax,v2i,B2i,fnneigh
+ real, dimension(npart) :: dummy
 !
 !--allow for tracing flow
 !      
- IF (trace) WRITE(iprint,*) ' Entering subroutine write_header'
+ if (trace) write(iprint,*) ' entering subroutine write_header'
 
 !-----------------------------------------------------------------------
 ! 1st header after options have been read, but before particle setup
 !-----------------------------------------------------------------------
 
- IF (icall.EQ.1) THEN
+ if (icall.eq.1) then
 !
 !--write version to file (for making backups of this code version)
 !
-    OPEN(UNIT=ireadf,file='version',status='replace')
-       WRITE(ireadf,"(a)") TRIM(version)
-    CLOSE(UNIT=ireadf)
+    open(unit=ireadf,file='version',status='replace')
+       write(ireadf,"(a)") trim(version)
+    close(unit=ireadf)
 !
-!--Write parameters to screen/logfile
+!--write parameters to screen/logfile
 !     
-    WRITE (iprint,10) version
-    IF (iprint.NE.6) WRITE (*,10) version
+    write (iprint,10) version
+    if (iprint.ne.6) write (*,10) version
      
-10 FORMAT(                                                                &
+10 format(                                                                &
       "                                                  _         _ ",/,  &
       "    ___ _   _ _ __   ___ _ __ ___ _ __  _ __ ___ | |__   __| |",/,  &
       "   / __| | | | '_ \ / _ \ '__/ __| '_ \| '_ ` _ \| '_ \ / _` |",/,  &
@@ -70,34 +71,34 @@ SUBROUTINE write_header(icall,infile,evfile,logfile)
       '                                                                     ',/,  &
       /,' Version: ',a30,/)
       
-    CALL date_and_time(startdate,starttime)
+    call date_and_time(startdate,starttime)
     startdate = startdate(7:8)//'/'//startdate(5:6)//'/'//startdate(1:4)
     starttime = starttime(1:2)//':'//starttime(3:4)//':'//starttime(5:)
-    WRITE(iprint,"(' Run started on ',a,' at ',a)") startdate,starttime
+    write(iprint,"(' Run started on ',a,' at ',a)") startdate,starttime
     
-    WRITE(iprint, 20) trim(infile),trim(evfile),trim(logfile)
-    IF (iprint.NE.6) WRITE(*, 20) trim(infile),trim(evfile),trim(logfile)
-20 FORMAT(/,                                &
+    write(iprint, 20) trim(infile),trim(evfile),trim(logfile)
+    if (iprint.ne.6) write(*, 20) trim(infile),trim(evfile),trim(logfile)
+20 format(/,                                &
       ' Read input from   : ',a,/,        &
       ' Writing energy to : ',a,/,        &
       ' Writing log to    : ',a,/)
 !
 !--time stepping options
 !
-    WRITE (iprint, 30) tmax, nmax, tout, nout
-30 FORMAT(' Maximum time   = ',f7.3, ' or ',i7,' timesteps',/,        &
+    write (iprint, 30) tmax, nmax, tout, nout
+30 format(' Maximum time   = ',f7.3, ' or ',i7,' timesteps',/,        &
              ' Output every t = ',f7.3, ' or ',i7,' timesteps',/)
 !
 !--number of dimensions
 !
-    WRITE (iprint, 40) ndim,ndimV
-40 FORMAT(' Number of spatial dimensions = ',i1,                &
+    write (iprint, 40) ndim,ndimV
+40 format(' Number of spatial dimensions = ',i1,                &
              ', velocity varies in ',i1,' dimensions.',/)
 !
 !--general options on form of equations
 !
-    WRITE (iprint,50) iener, icty, iprterm, iav, imhd, iexternal_force
-50 FORMAT(' Options: ',/,                                                &
+    write (iprint,50) iener, icty, iprterm, iav, imhd, iexternal_force
+50 format(' Options: ',/,                                                &
       6x,' Energy equation  : ',i2,5x,' Continuity Equation  :',i2,/,        &
       6x,' Pressure term    : ',i2,5x,' Artificial viscosity :',i2,/,        &
       6x,' Magnetic fields  : ',i2,5x,' External forces      :',i2/)
@@ -119,49 +120,49 @@ SUBROUTINE write_header(icall,infile,evfile,logfile)
 60  format(' Self gravity is ',a)
 65  format(' Self gravity is ',a,': h =',1pe9.2,/)
 
-    IF (imhd.NE.0) THEN
-       WRITE (iprint,70) imagforce,idivBzero,ianticlump,eps,neps
-70  FORMAT(' MHD options: ',/,                                        &
+    if (imhd.ne.0) then
+       write (iprint,70) imagforce,idivBzero,ianticlump,eps,neps
+70  format(' MHD options: ',/,                                        &
          6x,' Lorentz force    : ',i2,5x,' div B correction  :',i2,/,        &
          6x,' Artificial stress: ',i2,5x,' with eps = ',f4.2,', neps = ',i2,/)
- ENDIF
+ endif
 !
 !--artificial viscosity options
 !     
-    IF (iav.NE.0) THEN
-       WRITE (iprint,80) alphamin,alphaumin,alphaBmin,beta, &
+    if (iav.ne.0) then
+       write (iprint,80) alphamin,alphaumin,alphaBmin,beta, &
                          iavlim(:),avdecayconst,avfact
-80  FORMAT(' Artificial dissipative terms: ',/,                        &
+80  format(' Artificial dissipative terms: ',/,                        &
          6x,' alpha (min) = ',f6.2,f6.2,f6.2,' beta = ',f6.2,/,                &
          6x,' viscosity limiter   : ',i2,/, &
          6x,' conduction limiter  : ',i2,/, &
          6x,' resistivity limiter : ',i2,/, &
          6x,' decay constant = ',f6.2,', av source term x',f10.6, /)
-    ENDIF
+    endif
 !
 !--general constants
 !     
-    WRITE (iprint, 90) gamma
-90 FORMAT(' Equation of state: gamma = ',f10.6,/)
+    write (iprint, 90) gamma
+90 format(' Equation of state: gamma = ',f10.6,/)
 
 !
 !--timestepping
 !     
-    WRITE (iprint, 100) C_cour,C_force
-100 FORMAT(' Timestepping conditions: C_cour = ',f4.2,', C_force = ',f4.2,/)
+    write (iprint, 100) C_cour,C_force
+100 format(' Timestepping conditions: C_cour = ',f4.2,', C_force = ',f4.2,/)
 
-    WRITE (iprint,"(' Particle setup: ',/)")
+    write (iprint,"(' Particle setup: ',/)")
 
 !----------------------------------------------
 ! 2nd header after particles have been setup
 !----------------------------------------------
 
- ELSEIF (icall.EQ.2) THEN
+ elseif (icall.eq.2) then
  
 !
 !--number of particles
 !     
-    WRITE (iprint,"(/,' Number of particles = ',i9,/)") npart
+    write (iprint,"(/,' Number of particles = ',i9,/)") npart
 !
 !--boundary options
 !
@@ -188,20 +189,20 @@ SUBROUTINE write_header(icall,infile,evfile,logfile)
        coord(3) = 'z'
     end select
     
-    DO i=1,ndim
-       boundtype = 'unknown'
-       IF (ibound(i).EQ.0) boundtype = 'None'
-       IF (ibound(i).EQ.1) boundtype = 'Fixed particles'
-       IF (ibound(i).EQ.2) boundtype = 'Reflective ghosts'
-       IF (ibound(i).EQ.3) boundtype = 'Periodic (ghosts)'     
-       IF (ibound(i).EQ.5) boundtype = 'Shearing box (ghosts)'
-       WRITE (iprint, 210) coord(i),TRIM(boundtype)             
-       IF (ibound(i).NE.0) WRITE (iprint, 220) coord(i),xmin(i),coord(i),xmax(i)
-    ENDDO
-    WRITE (iprint, 230) nbpts
-210 FORMAT(1x,a1,' boundary:',1x,a)
-220 FORMAT(13x,a1,'min = ',f10.6,1x,a1,'max = ',f10.6)
-230 FORMAT(/,1x,'Number of fixed particles = ',i4,/)      
+    do i=1,ndim
+       boundtype = 'Unknown'
+       if (ibound(i).eq.0) boundtype = 'None'
+       if (ibound(i).eq.1) boundtype = 'Fixed particles'
+       if (ibound(i).eq.2) boundtype = 'Reflective ghosts'
+       if (ibound(i).eq.3) boundtype = 'Periodic (ghosts)'     
+       if (ibound(i).eq.5) boundtype = 'Shearing box (ghosts)'
+       write (iprint, 210) coord(i),trim(boundtype)             
+       if (ibound(i).ne.0) write (iprint, 220) coord(i),xmin(i),coord(i),xmax(i)
+    enddo
+    write (iprint, 230) nbpts
+210 format(1x,a1,' boundary:',1x,a)
+220 format(13x,a1,'min = ',f10.6,1x,a1,'max = ',f10.6)
+230 format(/,1x,'Number of fixed particles = ',i4,/)      
 !
 !--print out smoothing length information
 !
@@ -214,52 +215,52 @@ SUBROUTINE write_header(icall,infile,evfile,logfile)
        fNneigh = 4./3.*pi*(radkern*hfact)**3
     end select
     
-    WRITE (iprint,240) ihvar, ikernav, hfact, rhomin, ndim, tolh, fNneigh
-240 FORMAT(' Variable smoothing length: ',/,                                &
+    write (iprint,240) ihvar, ikernav, hfact, rhomin, ndim, tolh, fNneigh
+240 format(' Variable smoothing length: ',/,                                &
       6x,' h varied using method : ',i2,4x,' Kernel averaging :',i2,/,  &
       6x,' h = ',f4.2,'*[m/(rho + ',f7.5,')]^(1/',i1,'); htol = ',es8.2,/ &
       6x,' Number of neighbours = ',f8.2)
 !
 !--print out diagnostics of run
 !
-    WRITE(iprint,*)
-    CALL minmaxave(hh(1:npart),hmin,hmax,have,npart)
-    WRITE(iprint,250) 'h',hmin,hmax,have
-    CALL minmaxave(dens(1:npart),hmin,hmax,have,npart)
-    WRITE(iprint,250) 'dens',hmin,hmax,have
-    CALL minmaxave(uu(1:npart),hmin,hmax,have,npart)
-    WRITE(iprint,250) 'u',hmin,hmax,have
-    DO i=1,ndimV
-       CALL minmaxave(vel(i,1:npart),hmin,hmax,have,npart)
-       WRITE(iprint,250) 'v'//trim(coord(i)),hmin,hmax,have
-    ENDDO
+    write(iprint,*)
+    call minmaxave(hh(1:npart),hmin,hmax,have,npart)
+    write(iprint,250) 'h',hmin,hmax,have
+    call minmaxave(dens(1:npart),hmin,hmax,have,npart)
+    write(iprint,250) 'dens',hmin,hmax,have
+    call minmaxave(uu(1:npart),hmin,hmax,have,npart)
+    write(iprint,250) 'u',hmin,hmax,have
+    do i=1,ndimv
+       call minmaxave(vel(i,1:npart),hmin,hmax,have,npart)
+       write(iprint,250) 'v'//trim(coord(i)),hmin,hmax,have
+    enddo
 !--mach number
-    IF (iprterm.GE.0) THEN
-       DO i=1,npart
+    if (iprterm.ge.0) then
+       do i=1,npart
           v2i = dot_product(vel(:,i),vel(:,i))
           dummy(i) = sqrt(v2i)/spsound(i)
-       ENDDO
-       CALL minmaxave(dummy(1:npart),hmin,hmax,have,npart)
-       WRITE(iprint,250) 'Mach #',hmin,hmax,have
-    ENDIF    
-    IF (imhd.NE.0) THEN
-       DO i=1,ndimV
-          CALL minmaxave(Bfield(i,1:npart),hmin,hmax,have,npart)
-          WRITE(iprint,250) 'B'//trim(coord(i)),hmin,hmax,have
-       ENDDO
+       enddo
+       call minmaxave(dummy(1:npart),hmin,hmax,have,npart)
+       write(iprint,250) 'Mach #',hmin,hmax,have
+    endif    
+    if (imhd.ne.0) then
+       do i=1,ndimv
+          call minmaxave(bfield(i,1:npart),hmin,hmax,have,npart)
+          write(iprint,250) 'B'//trim(coord(i)),hmin,hmax,have
+       enddo
 !--plasma beta
-       DO i=1,npart
-          B2i = dot_product(Bfield(:,i),Bfield(:,i))
-          dummy(i) = pr(i)/(0.5*B2i)
-       ENDDO
-       CALL minmaxave(dummy(1:npart),hmin,hmax,have,npart)
-       WRITE(iprint,250) 'beta',hmin,hmax,have
-    ENDIF
+       do i=1,npart
+          b2i = dot_product(bfield(:,i),bfield(:,i))
+          dummy(i) = pr(i)/(0.5*b2i)
+       enddo
+       call minmaxave(dummy(1:npart),hmin,hmax,have,npart)
+       write(iprint,250) 'beta',hmin,hmax,have
+    endif
 
-250 FORMAT (1x,a6,' min = ',1pe9.2,' max = ',1pe9.2,' av = ',1pe9.2)
-    WRITE(iprint,*)
+250 format (1x,a6,' min = ',1pe9.2,' max = ',1pe9.2,' av = ',1pe9.2)
+    write(iprint,*)
 
- ENDIF
+ endif
       
-RETURN
-END SUBROUTINE write_header
+return
+end subroutine write_header
