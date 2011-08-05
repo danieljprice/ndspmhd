9255750be5dcfeff0070aa7c63d3d2b48d404f7a
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 00:33:33 +0000

    toro solver
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@2103 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/riemannsolver.f90
+++ src/riemannsolver.f90
@@ -1,72 +1,90 @@
 !-------------------------------------------------------------------
-! Implementation of the iterative Riemann solver of Van Leer (1997)
-! Follows Cha & Whitworth (2003), MNRAS 340, 73-90
+! Implementation of the exact Riemann solver given in Toro (1992)
+!
+! Solves for the post-shock pressure (pr) and velocity (vstar)
+! given the initial left and right states
+!
+! Does not matter if high P / high rho is on left or right
+!
+! Daniel Price, Institute of Astronomy, Cambridge, UK, 2004
+! dprice@ast.cam.ac.uk
 !-------------------------------------------------------------------
-subroutine riemannsolver(gamma,pra,prb,va,vb,ca,cb,pr,vstar)
+subroutine riemannsolver(gamma,p_L,p_R,v_L,v_R,c_L,c_R,pr,vstar)
   implicit none
-  real, parameter :: tol = 1.5e-2
-  real :: gamma,pra,prb,va,vb,ca,cb,pr,vstar
-  real :: wa,wb,za,zb,vstara,vstarb,prnew
+  real, parameter :: tol = 1.5e-4
+  real, intent(in) :: gamma,p_L,p_R,v_L,v_R,c_L,c_R
+  real, intent(out) :: pr,vstar
+  integer, parameter :: maxits = 30
+  integer :: its
+  real :: prnew, f_L, f_R, dfdp_L, dfdp_R, f, df, dp
+  real :: power, denom
 !
 !--get an initial starting estimate of intermediate pressure
+!  this one is from Toro(1992) - gives basically the right answer
+!  for pressure jumps below about 4
 !
-  prnew = (ca*prb + cb*pra - ca*cb*(va-vb))/(ca+cb)
-  pr = pra
+  power = (gamma-1.)/(2.*gamma)
+  denom = c_L/p_L**power + c_R/p_R**power
+  prnew = ((c_L + c_R + (v_L - v_R)*0.5*(gamma-1.))/denom)**(1./power)
+  pr = p_L
+  its = 0
 
-  do while (abs(prnew-pr).lt.tol)
+  !!print*,'initial guess = ',prnew
 
+  do while (abs(prnew-pr).gt.tol .and. its.lt.maxits)
+
+     its = its + 1
      pr = prnew
 !
-!--find Lagrangian shock speed and tangential slope
+!--evaluate the function and its derivatives
 !
-     wa = wspeed(gamma,pr,pra,ca)
-     wb = wspeed(gamma,pr,prb,cb)
-     za = zslope(wa,gamma,pr,pra,ca)
-     zb = zslope(wb,gamma,pr,prb,cb)
-!
-!--update vstar
-!
-     vstara = va + (pr-pra)/wa
-     vstarb = vb - (pr-prb)/wb
+     call f_and_df(pr,p_L,c_L,gamma,f_L,dfdp_L)
+     call f_and_df(pr,p_R,c_R,gamma,f_R,dfdp_R)
 !
 !--then get new estimate of pr
 !
-     prnew = pr - zb*za*(vstara - vstarb)/(za+zb)
-
+     f = f_L + f_R + (v_R - v_L)
+     df = dfdp_L + dfdp_R
+!
+!--Newton-Raphson iterations
+!     
+     dp =  -f/df
+     prnew = pr + dp
+     
   enddo
 
-  print*,'its = ',its,' pr = ',prnew,' pra,prb = ',pra,prb
+  if (its.eq.maxits) print*,'WARNING: its not converged in riemann solver'
   pr = prnew
-  vstar = (zb*vstarb + za*vstara)/(za+zb)
+  vstar = v_L - f_L
+
+!!  print*,'its = ',its,' pr = ',prnew,'v = ',vstar,v_R + f_R
 
 end subroutine riemannsolver
 
 !
-!--Lagrangian shock speed
+!--pressure function
+!  H is pstar/p_L or pstar/p_R
 !
-real function wspeed(gamma,pr,pra,ca)
+subroutine f_and_df(prstar,pr,cs,gam,fp,dfdp)
   implicit none
-  real :: gamma, pr, pra, ca, gamfac
+  real, intent(in) :: prstar, pr, gam, cs
+  real, intent(out) :: fp,dfdp
+  real :: H,term, power, gamm1, denom
+
+  H = prstar/pr
+  gamm1 = gam - 1.
   
-  if (pr.ge.pra) then
-     wspeed = ca*sqrt(1.+(gamma+1.)/(2.*gamma)*(pr-pra)/pra)
-  else
-     gamfac = gamma-1./(2.*gamma)
-     wspeed = ca*gamfac*(1.-pr/pra)/(1.-(pr/pra)**gamfac)
+  if (H.gt.1.) then  ! shock
+     denom = gam*((gam+1.)*H + gamm1)
+     term = sqrt(2./denom)
+     fp = (H - 1.)*cs*term
+         
+     dfdp = cs*term/pr + (H - 1.)*cs/term*(-1./denom**2)*gam*(gam+1.)/pr
+  else               ! rarefaction
+     power = gamm1/(2.*gam)
+     fp = (H**power - 1.)*(2.*cs/gamm1)
+  
+     dfdp = 2.*cs/gamm1*power*H**(power-1.)/pr
   endif
   
-end function wspeed
-!
-!--tangential slope
-!  
-real function zslope(wa,gamma,pr,pra,ca)
-  implicit none
-  real :: gamma,pr,pra,ca,wa,w2
-  
-  if (pr.ge.pra) then
-     w2 = wa*wa
-     zslope = 2.*w2/(w2 + ca**2)*wa
-  else
-     zslope = ca*(pr/pra)**(1-(gamma-1)/(2.*gamma))
-  endif
-end function zslope
+end subroutine f_and_df
