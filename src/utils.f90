321900be455a19cd21c4b1f39211f4b49fdc0ddb
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 00:24:07 +0000

    various utilities
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@1899 cab04810-efc7-4a10-8ecf-f366c833a2ad

---
+++ src/utils.f90
@@ -0,0 +1,22 @@
+!
+!--simple routine to take min, max and average of a quantity
+!
+subroutine minmaxave(x,xmin,xmax,xav,npts)
+  implicit none
+  integer :: i
+  integer, intent(in) :: npts
+  real, intent(in), dimension(npts) :: x
+  real, intent(out) :: xmin,xmax,xav
+  
+  xav = 0.
+  xmin = 1.e10
+  xmax = -1.e10
+  do i=1,npts
+     xav = xav + x(i)
+     xmin = min(xmin,x(i))
+     xmax = max(xmax,x(i))
+  enddo
+  xav = xav/real(npts)
+
+  return
+end subroutine minmaxave
