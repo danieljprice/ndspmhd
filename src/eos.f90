f0e997a24a9b899b02cfa58cd7854719eb24deb6
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 00:27:18 +0000

    works on arrays
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@1963 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/eos.f90
+++ src/eos.f90
@@ -2,43 +2,59 @@
 !! This subroutine returns the prsure and sound speed
 !! from rho and/or u_therm via the equation of state
 !!
-!! This version does one variable at a time
+!! size of array is specified on input, so can send in either whole
+!! arrays or individual elements (just be consistent!)
 !!-----------------------------------------------------------------
 
-SUBROUTINE equation_of_state(pr,vsound,uu,rho,gamma)
- USE dimen_mhd
+SUBROUTINE equation_of_state(pr,vsound,uu,rho,gamma,isize)
  USE options
  USE loguns
  USE polyconst
+!
+!--define local variables
+!
  IMPLICIT NONE
- REAL, INTENT(IN) :: gamma,rho
- REAL, INTENT(OUT) :: pr
- REAL, INTENT(INOUT) :: uu,vsound
- REAL :: gamma1
+ INTEGER :: i
+ INTEGER, INTENT(IN) :: isize
+ REAL, INTENT(IN) :: gamma
+ REAL, INTENT(IN), DIMENSION(isize) :: rho
+ REAL, INTENT(OUT), DIMENSION(isize) :: pr
+ REAL, INTENT(INOUT), DIMENSION(isize) :: uu,vsound
+ REAL :: gamma1,B2
 
  gamma1 = gamma - 1.
 !
-!--check for errors
+!--exit gracefully if rho is negative
 !
- IF (uu.LT.0. .or. rho.LE.0.) THEN
-    WRITE(iprint,*) 'Error: eos: rho,uu = ',rho,uu
+ IF (ANY(rho.LT.0.)) THEN
+    WRITE(iprint,*) 'eos: rho -ve, exiting'
+    DO i=1,isize
+       IF (rho(i).LT.0.) WRITE(iprint,*) i,rho(i),uu(i)
+    ENDDO
     CALL quit
+ ELSEIF ((iener.NE.0).AND.ANY(uu.LT.0.)) THEN
+    WRITE(iprint,*) 'eos: u_therm -ve, exiting'
+    DO i=1,isize
+       IF (uu(i).LT.0.) WRITE(iprint,*) i,rho(i),uu(i)
+    ENDDO    
+    CALL quit      
  ENDIF
-!
-!--polytropic (isothermal when gamma=1)
-! 
- IF (iener.EQ.0) THEN
-    pr = polyk*rho**gamma
-    vsound = SQRT(gamma*pr/rho)
+
+ IF (iener.EQ.0) THEN	! polytropic (isothermal when gamma=1)
+    WHERE (rho > 0.)
+      pr = polyk*rho**gamma
+      vsound = SQRT(gamma*pr/rho)
+    END WHERE  
     IF (ABS(gamma1).GT.1.e-3) THEN
+       WHERE (rho > 0.) 
        uu = pr/(gamma1*rho)    
+       END WHERE
     ENDIF   
- ELSE		
-!
-!--adiabatic
-!
-    pr = gamma1*uu*rho
-    vsound = SQRT(gamma*pr/rho)
+ ELSE		! adiabatic
+    WHERE (rho > 0.)
+      pr = gamma1*uu*rho
+      vsound = SQRT(gamma*pr/rho)
+    END WHERE  
  ENDIF
       
  RETURN
