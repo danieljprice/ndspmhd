1f446e0ae240df207a58fed7f610eb6d9c1590eb
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 01:55:18 +0000

    force error routine for double-plummer-sphere
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@3295 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/setup_densityprofileND.f90
+++ src/setup_densityprofileND.f90
@@ -2,6 +2,12 @@
 !     Set up an arbitrary (radial) density profile
 !     using random placement of particles
 !----------------------------------------------------------------
+module plummer_setup
+ implicit none
+ real :: mass1,mass2,rsoft,rsoft2,xdist2
+ 
+end module plummer_setup
+
 
 subroutine setup
 !
@@ -16,6 +22,7 @@ subroutine setup
  use geometry, only:coord_transform
  use timestep, only:iseedMC
  use mem_allocation, only:alloc
+ use plummer_setup
  
  use uniform_distributions
 !
@@ -24,7 +31,7 @@ subroutine setup
  implicit none
  integer :: i,iseed,idist,j,npart1,npart2
  real :: massp,totmass,ran2,massratio
- real :: rr,rmass,rsoft,rsoft2,rdist2
+ real :: rr,rmass
  real, dimension(ndim) :: xpos
  real :: dxij,dxmean,q,vmax,vesc,vr,rr2
  logical, parameter :: isetvelocities = .false.
@@ -42,7 +49,7 @@ subroutine setup
 !
 !--set up the uniform density grid
 !
- npart = 10000
+ npart = 1000
 ! npart = int((1./psep)**3)
 ! print*,'enter npart'
 ! read*,npart
@@ -80,13 +87,15 @@ subroutine setup
  massratio = 0.5  ! 1.0 for no second component, 0.0 for satellite only
  npart1 = int(massratio*npart)
  npart2 = npart - npart1
+ mass1 = massratio*totmass
+ mass2 = totmass*(1.-massratio)
  if (npart1.ne.npart) then
     rsoft2 = 0.1   ! scale length for 2nd component
-    rdist2 = 0.0  ! distance to 2nd component (in x)
+    xdist2 = 10.0  ! distance to 2nd component (in x)
     write(iprint,*) ' + smaller satellite, mass ratio = ',massratio
-    write(iprint,*) ' mass (1) = ',massratio*totmass,' mass(2) = ',totmass*(1.-massratio)
+    write(iprint,*) ' mass (1) = ',mass1,' mass(2) = ',mass2
     write(iprint,*) ' npart (1) = ',npart1,' npart(2) = ',npart2
-    write(iprint,*) ' rsoft (2) = ',rsoft2,' separation = ',rdist2
+    write(iprint,*) ' rsoft (2) = ',rsoft2,' separation = ',xdist2
     call getr_from_m(idist,0.99,rsoft2,rr)
     write(iprint,*) ' Cutoff radius (2) = ',rr
  endif
@@ -127,7 +136,7 @@ subroutine setup
        dens(i) = densprofile(idist,rr,rsoft)
     else
        !--offset 2nd component by specified amount (in x only)
-       x(1,i) = x(1,i) + rdist2 
+       x(1,i) = x(1,i) + xdist2 
        dens(i) = densprofile(idist,rr,rsoft2)
     endif
     pmass(i) = massp
@@ -283,3 +292,95 @@ real function distfn(q,ioption)
 end function distfn
 
 end subroutine setup
+
+!
+!--gives solution for all 3 force components for two plummer spheres at differing locations
+!
+subroutine force_error_densityprofiles(iprofile,npart,xpts,force,residual,errL2,errL1,ierr)
+  use plummer_setup
+  implicit none
+  real, parameter :: pi = 3.1415926536
+  integer, intent(in) :: iprofile,npart
+  real, intent(in), dimension(3,npart) :: xpts,force
+  real, intent(out), dimension(npart) :: residual
+  real, intent(out) :: errL2,errL1
+  integer, intent(out) :: ierr
+  integer :: i
+  real, dimension(3) :: x1,x2,dx1,dx2,df,fexacti
+  real :: err2,err1,fmax
+!
+! check for errors
+!
+  ierr = 0
+  if (rsoft.lt.0.) then
+     print*,'error: rsoft < 0 in exact_densityprofile'
+     ierr = 3
+     return
+  endif
+  
+  errL1 = 0.
+  errL2 = 0.
+  fmax = 0.
+  print*,'npart = ',npart,'masses = ',mass1,mass2
+
+  select case(iprofile)  
+  case(1)
+!
+!--Plummer sphere
+!
+    !--position of 1st component
+    x1 = (/0.0,0.0,0.0/)
+    !--position of 2nd component
+    x2 = (/0.0,0.0,0.0/)
+    x2(1) = xdist2
+
+!--potential
+!       do i=1,npart
+!          yplot(1,i) = -mass1/sqrt(rsoft**2 + dot_product(dx1,dx1)**2) &
+!                       -mass2/sqrt(rsoft2**2 + dot_product(dx2,dx2)**2)
+!       enddo
+!--force
+    do i=1,npart
+       dx1(:) = xpts(:,i) - x1(:)
+       dx2(:) = xpts(:,i) - x2(:)
+       fexacti(1) = -mass1*dx1(1)/ &
+                    ((rsoft**2 + dot_product(dx1,dx1))**1.5) &
+                  - mass2*dx2(1)/ &
+                    ((rsoft2**2 + dot_product(dx2,dx2))**1.5)
+       fexacti(2) =-mass1*dx1(2)/ &
+                    ((rsoft**2 + dot_product(dx1,dx1))**1.5) &
+                  - mass2*dx2(2)/ &
+                    ((rsoft2**2 + dot_product(dx2,dx2))**1.5)
+       fexacti(3) = -mass1*dx1(3)/ &
+                    ((rsoft**2 + dot_product(dx1,dx1))**1.5) &
+                  - mass2*dx2(3)/ &
+                    ((rsoft2**2 + dot_product(dx2,dx2))**1.5)
+       df = fexacti(:) - force(:,i)
+!       if (i.lt.10) then
+!          print*,'force = ',force(:,i), ' fexact = ',fexacti(:)
+!       endif
+       fmax = max(fmax,maxval(abs(fexacti(:))))
+       err2 = dot_product(df,df)
+       err1 = sqrt(err2)
+       residual(i) = err1/sqrt(dot_product(fexacti(:),fexacti(:)))
+       errL2 = errL2 + err2
+       errL1 = errL1 + err1
+    enddo
+
+    !
+    !--normalise errors (use maximum f value)
+    !
+    print*,'fmax = ',fmax
+    if (fmax.gt.tiny(fmax)) then
+       errL1 = errL1/(npart*fmax)
+       errL2 = errL2/(npart*fmax**2)
+    else
+       stop 'error normalising errors'
+    endif
+
+  case default
+     ierr = 1  
+  end select
+    
+  return
+end subroutine force_error_densityprofiles
