2b56331d72ebdac427928c6cd16e408cdbb041cb
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 01:09:56 +0000

    various crap
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@2515 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- multi/multirun.f90
+++ multi/multirun.f90
@@ -1,117 +1,97 @@
 !!-----------------------------------------------------------------------
 !! computes multiple infiles where one or several parameters are varied
 !!
-!! this varies h and the div B cleaning parameters
+!! this one does toy star oscillations in 2D
 !!-----------------------------------------------------------------------
-PROGRAM multirun
- USE dimen_mhd
- USE loguns
- USE artvi
- USE eos
- USE options
- USE setup_params
- USE timestep
- USE xsph
- USE anticlumping
+program multirun
+ use dimen_mhd
+ use loguns
+ use artvi
+ use eos
+ use options
+ use setup_params
+ use timestep
+ use xsph
+ use anticlumping
  
- USE infiles
- IMPLICIT NONE
- INTEGER :: i,j,nruns,iener_default
- INTEGER :: ierr
- CHARACTER :: filename*15,infile*20,filenum*2,charnruns*3,shkfile*20
- REAL :: rholeft,rhoright,prleft,prright
- REAL :: vxleft,vxright,vyleft,vyright,vzleft,vzright
- REAL :: Bxinit,Byleft,Byright,Bzleft,Bzright,const
+ use infiles
+ implicit none
+ integer :: i,j,nruns,jmode,mmode
+ integer :: ierr
+ character :: filename*15,infile*20,tstarfile*20,filenum*2,charnruns*3
+ real :: h,c,a,sigma2,sigma,gamm1,period,omegasq
  
  nruns = 0
  iread = 11
- CALL getarg(1,filename)
- CALL getarg(2,charnruns)
+ call getarg(1,filename)
+ call getarg(2,charnruns)
  
  read(charnruns,*,iostat=ierr) nruns
  
- IF (filename.EQ.'' .OR. ierr.NE.0) THEN
-  PRINT*,'Usage: multirun filename nruns'
-  STOP
- ENDIF
+ if (filename.eq.'' .or. ierr.ne.0) then
+  print*,'usage: multirun filename nruns'
+  stop
+ endif
  
- PRINT*,' filename = ',filename,' nruns = ',nruns 
-  
+ print*,' filename = ',filename,' nruns = ',nruns
 !
 !--set default options
 !
- CALL set_default_options
+ call set_default_options
+
 !
-!-- or read the generic input file
+!--read the generic input file
 ! 
- PRINT*,' reading multirun.in... '
- CALL read_infile('multirun.in')
- PRINT*,' initial psep = ',psep
- iener_default = iener
- 
- psidecayfact = 1.0
- !hfact = 1.0
- j = 0
-
- const = 1./SQRT(4.*pi)
-
-       psep = 0.0025
-       gamma = 1.66666666666667
-       tmax = 0.08
-       tout = 0.04
-       
-       rholeft = 1.0
-       rhoright = 1.0
-       prleft = 20.0
-       prright = 1.0
-       vxleft = 10.0
-       vxright = -10.0
-       vyleft = 0.
-       vyright = 0.
-       vzleft = 0.
-       vzright = 0.
-       Bxinit = 5.*const
-       Byleft = 5.*const
-       Byright = 5.*const
-       Bzleft = 0.
-       Bzright = 0.
-        
- DO i=1,nruns
-    IF (i.GE.10) THEN
-       filenum = ACHAR(48+i/10)//ACHAR(48+mod(i,10))
-       infile = TRIM(filename)//filenum//'.in'
-       shkfile = TRIM(filename)//filenum//'.shk'
-    ELSE
-       filenum(1:1) = ACHAR(48+mod(i,10))
-       infile = TRIM(filename)//filenum(1:1)//'.in'
-       shkfile = TRIM(filename)//filenum(1:1)//'.shk'
-    ENDIF
+ print*,' reading multirun.in... '
+ call read_infile('multirun.in')
+ print*,' initial psep = ',psep
+  
+ jmode = 0
+ mmode = 0 
+ do i=1,nruns
+    if (i.ge.10) then
+       filenum = achar(48+i/10)//achar(48+mod(i,10))
+       infile = trim(filename)//filenum//'.in'
+       tstarfile = trim(filename)//filenum//'.tstar'
+    else
+       filenum(1:1) = achar(48+mod(i,10))
+       infile = trim(filename)//filenum(1:1)//'.in'
+       tstarfile = trim(filename)//filenum(1:1)//'.tstar'
+    endif
+    print*,' writing input file ',infile, ' psep = ',psep
     
-    j = j + 1
-    !IF (j.GT.9) THEN
-    !   j = 0
-    !   psep = 0.5*psep
-    !   psidecayfact = -0.1
-    !ENDIF
-    !psidecayfact = psidecayfact + 0.1
-    !hfact = hfact + 0.2
+    h = 1.0
+    c = 1.0
+    a = 0.05/sqrt(2.)
+    jmode = jmode + 2
 
-    PRINT*,'Writing ',shkfile,' with initial left/right states'
-    OPEN(UNIT=11,FILE=shkfile,STATUS='replace',FORM='formatted')
-       WRITE(11,*) rholeft,rhoright
-       WRITE(11,*) prleft,prright
-       WRITE(11,*) vxleft,vxright
-       WRITE(11,*) vyleft,vyright              
-       WRITE(11,*) vzleft,vzright
-       WRITE(11,*) Bxinit
-       WRITE(11,*) Byleft,Byright
-       WRITE(11,*) Bzleft,Bzright       
-    CLOSE(UNIT=11)
-        
-    PRINT*,'Writing input file ',infile, ' psep = ',psep,' sigma = ',psidecayfact,' hfact = ',hfact
+    gamm1 = gamma - 1.
+    if (gamm1.lt.1.e-5) then
+       stop 'error: gamma - 1 <= 0'
+    endif
+
+    omegasq = 1.0
+    sigma2 = 0.5*omegasq*(gamm1)*((jmode+mmode)*(jmode+mmode + 2./gamm1) - mmode**2)
+    if (sigma2.lt.1.e-5) then
+       print*,'ERROR sigma2 < 0 in perturbation'
+    else
+       sigma = sqrt(sigma2)
+    endif
+!
+!--run for 10 periods with output every period
+!
+    period = 2.*pi/sigma
+    tout = 0.25*period
+    tmax = 10.*period
+    print*,'jmode = ',jmode,' smode = ',mmode,' period = ',period
     
-    CALL write_infile(infile)
+    print*,'writing ',tstarfile,' with initial left/right states'
+    open(unit=11,file=tstarfile,status='replace',form='formatted')
+       write(11,*) jmode,mmode
+    close(unit=11)
+    
+    call write_infile(infile)
 
- ENDDO
+ enddo
  
-END PROGRAM multirun
+end program multirun
