e88a0dc566375de204b9dede66a6cb1573ccf021
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 02:35:21 +0000

    utils now a module
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@4326 cab04810-efc7-4a10-8ecf-f366c833a2ad

--- src/utils.f90
+++ src/utils.f90
@@ -2,7 +2,10 @@
 ! This file contains some simple utilities used in various places
 ! throughout the code
 !------------------------------------------------------------------
+module utils
+ public :: minmaxave,cross_product3D,curl3D_epsijk
 
+contains
 !-------------------------------------------------------------------
 ! simple routine to take min, max and average of a quantity
 !-------------------------------------------------------------------
@@ -26,7 +29,7 @@ subroutine minmaxave(x,xmin,xmax,xav,npts)
   return
 end subroutine minmaxave
 
-subroutine cross_product3D(veca,vecb,vecc)
+pure subroutine cross_product3D(veca,vecb,vecc)
  implicit none
  real, dimension(3), intent(in) :: veca,vecb
  real, dimension(3), intent(out) :: vecc
@@ -37,7 +40,7 @@ subroutine cross_product3D(veca,vecb,vecc)
 
 end subroutine cross_product3D
 
-subroutine curl3D_epsijk(gradAvec,curlA)
+pure subroutine curl3D_epsijk(gradAvec,curlA)
  implicit none
  real, dimension(3,3), intent(in) :: gradAvec
  real, dimension(3), intent(out) :: curlA
@@ -47,3 +50,43 @@ subroutine curl3D_epsijk(gradAvec,curlA)
  curlA(3) = gradAvec(1,2) - gradAvec(2,1)
 
 end subroutine curl3D_epsijk
+
+!----------------------------------------------------------------
+!+
+!  Internal subroutine that inverts a 3x3 matrix
+!+
+!----------------------------------------------------------------
+pure subroutine matrixinvert3D(A,Ainv,ierr)
+ implicit none
+ real, intent(in), dimension(3,3) :: A
+ real, intent(out), dimension(3,3) :: Ainv
+ integer, intent(out) :: ierr
+ real, dimension(3) :: x0,x1,x2,result
+ real    :: det, ddet
+ 
+ x0 = A(1,:)
+ x1 = A(2,:)
+ x2 = A(3,:)
+ 
+ call cross_product3D(x1,x2,result)
+ det = dot_product(x0,result)
+ 
+ if (abs(det).gt.tiny(det)) then
+    ddet = 1./det
+ else
+    ddet = 0.
+    Ainv = 0.
+    ierr = 1
+    return
+ endif
+
+ Ainv(1,:) = result(:)*ddet
+ call cross_product3D(x2,x0,result)
+ Ainv(2,:) = result(:)*ddet
+ call cross_product3D(x0,x1,result)
+ Ainv(3,:) = result(:)*ddet
+
+ return
+end subroutine matrixinvert3D
+
+end module utils
