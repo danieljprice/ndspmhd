36861ba414c79dcce083a2bde68c072f3d59b6e4
dprice <dprice@cab04810-efc7-4a10-8ecf-f366c833a2ad>
2011-08-05 00:16:15 +0000

    changes file options
    git-svn-id: https://svn-vre.its.monash.edu.au/mathsci/ndspmhd/trunk@1792 cab04810-efc7-4a10-8ecf-f366c833a2ad

---
+++ multi/multi_options.f90
@@ -0,0 +1,136 @@
+!!-----------------------------------------------------------------------
+!! computes multiple infiles where one or several parameters are varied
+!! this version changes equation types etc
+!!-----------------------------------------------------------------------
+PROGRAM multirun
+ USE dimen_mhd
+ USE loguns
+ USE artvi
+ USE eos
+ USE options
+ USE polyconst
+ USE setup_params
+ USE timestep
+ USE xsph
+ USE anticlumping
+ IMPLICIT NONE
+ INTEGER :: i,j,nruns
+ INTEGER :: int_from_string 
+ CHARACTER :: filename*15,infile*20,filenum*2,charnruns*3
+ 
+ nruns = 0
+ iread = 11
+ CALL getarg(1,filename)
+ CALL getarg(2,charnruns)
+ 
+ nruns = int_from_string(charnruns)
+ 
+ IF (filename.EQ.'') THEN
+  PRINT*,' Enter filename for runs'
+  READ*,filename
+ ENDIF
+ 
+ IF (nruns.LE.0) THEN
+  PRINT*,' Enter number of runs:'
+  READ*,nruns
+ ENDIF
+ 
+ PRINT*,' filename = ',filename,' nruns = ',nruns 
+  
+! PRINT*,' Enter particle separation run 1 '
+! READ*,psep
+!
+!--set all options (either set them all here or read from an infile)
+! 
+ tmax = 10.0
+ tout = 1.0
+ nmax = 1000000
+ nout = -1
+ gamma = 1.6666666666666666666666667
+ iener = 2
+ gconst = 0.0
+ polyk = 0.6
+ icty = 0
+ ndirect = nmax
+ ialtform = 0
+ iav = 1
+ alphamin = 0.1
+ beta = 2.0
+ iavlim = 1
+ avconst = 0.1
+ ikernav = 3
+ ihvar = 2
+ hfact = 1.5
+ idumpghost = 0
+ imhd = 11
+ imagforce = 2
+ idivBzero = 0
+ ianticlump = 1
+ eps = 0.8
+ neps = 5
+ ixsph = 0
+ xsphfac = 0.5
+!
+!-- or read the generic input file
+! 
+ PRINT*,' reading multirun.in... '
+ CALL read_infile('multirun.in')
+ PRINT*,' initial psep = ',psep
+ 
+ DO i=1,nruns
+    IF (i.GE.10) THEN
+       filenum = ACHAR(48+i/10)//ACHAR(48+mod(i,10))
+       infile = TRIM(filename)//filenum//'.in'
+    ELSE
+       filenum(1:1) = ACHAR(48+mod(i,10))
+       infile = TRIM(filename)//filenum(1:1)//'.in'
+    ENDIF
+    SELECT CASE(i)
+      CASE(1)
+         iener = 3
+	 gconst = 0.25
+	 imhd = 11
+	 ikernav = 3
+	 ihvar = 2
+      CASE(2)
+         iener = 3
+	 gconst = 0.25
+	 imhd = 1
+	 ikernav = 3
+	 ihvar = 2	 
+      CASE(3)
+         iener = 2
+	 imhd = 11
+         gconst = 0.25
+	 ikernav = 3
+	 ihvar = 2
+      CASE(4)
+         iener = 2
+	 imhd = 1
+         gconst = 0.25
+	 ikernav = 3
+	 ihvar = 2	 	 
+      CASE(5)
+         iener = 3
+	 gconst = 0.25
+	 imhd = 1
+	 ikernav = 2
+	 ihvar = 2     
+      CASE(6)
+         iener = 2
+	 gconst = 0.25
+	 imhd = 1
+	 ikernav = 2
+	 ihvar = 2     
+      CASE(7)
+         iener = 3
+	 gconst = 0.25
+	 imhd = 11
+	 ikernav = 2
+	 ihvar = 2        
+    END SELECT
+    PRINT*,' writing input file ',infile
+    CALL write_infile(infile)
+ ENDDO
+ 
+END PROGRAM multirun
