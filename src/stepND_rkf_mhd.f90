c7ab10e73ca74bbbc76b23ed0e8c431067c872fa
Daniel Price <daniel.price@monash.edu>
2014-05-29 17:46:49 +1000

    implemented MHD in rkf integrator; various bugs fixed with this

--- src/stepND_rkf_mhd.f90
+++ src/stepND_rkf_mhd.f90
@@ -71,21 +71,22 @@ subroutine step ()
 !--define local variables
 !
  implicit none
- integer :: i,j,nsplit, ii
+ integer :: i,ii
  real, dimension(ndimV,npart) :: forcein,dBevoldtin
  real, dimension(npart) :: drhodtin,dhdtin,dendtin,uuin,dpsidtin
  real, dimension(3,npart) :: daldtin
  real :: hdt, dtin
- real, dimension(ndim)  :: xcyl,velcyl
- real, dimension(ndimV) :: vcrossB
- real, dimension(ndim) :: xtemp
- real, dimension(ndimV) :: vtemp
+ real, dimension(ndim)  :: xtemp
+ real, dimension(ndimV) :: vtemp, Btemp
  real :: dtmin, dtmax, dtnext
- real :: rkferr, rkferrratio, dtfactor
+ real :: rkferr, rkferrratio, dtfactor, errmhd
  logical :: loop
  real, dimension(ndimV,npart) :: v1,v2,v3,v4,v5,v6
  real, dimension(ndimV,npart) :: f1,f2,f3,f4,f5,f6
+ real, dimension(ndimV,npart) :: dB1,dB2,dB3,dB4,dB5,dB6
  real, dimension(npart) :: dh1,dh2,dh3,dh4,dh5,dh6
+ real, dimension(npart) :: den1,den2,den3,den4,den5,den6
+ real, dimension(npart) :: drho1,drho2,drho3,drho4,drho5,drho6
 
  loop = .true.
 
@@ -100,7 +101,6 @@ subroutine step ()
 !
 !--set initial quantities
 !
- hdt = 0.5*dt
 ! print *, 'DT = ', dt
  dtin = dt
  dtnext = huge(dtnext)
@@ -124,12 +124,10 @@ subroutine step ()
     daldtin(:,i) = daldt(:,i)
     dpsidtin(i) = dpsidt(i)
  enddo
-!
-!--if doing divergence correction then do correction to magnetic field
-! 
- if (idivbzero.eq.10) call divBcorrect(npart,ntotal)
- if (idivbzero.eq.10) call divBcorrect(npart,ntotal)
- if (idivbzero.eq.10) call divBcorrect(npart,ntotal)
+
+ if (any(iavlim.ne.0)) stop 'rkf not implemented with alpha switches'
+ if (idivBzero.ge.2) stop 'rkf not implemented with div B correction'
+ if (ihvar.eq.1) stop 'ihvar=1 not implemented with rkf'
 
 !--Loop until RKF timestep gives convergence
 
@@ -141,9 +139,14 @@ subroutine step ()
  ! xin
  ! velin
  ! forcein
- v1(:,:) = velin(:,:)
- f1(:,:) = forcein(:,:)
- dh1 = dhdtin(:)
+ do i=1,npart
+    v1(:,i)  = velin(:,i)
+    f1(:,i)  = forcein(:,i)
+    dB1(:,i) = dBevoldt(:,i)
+    dh1(i)   = dhdtin(i)
+    den1(i)  = dendt(i)
+    drho1(i) = drhodt(i)
+ enddo
 !
 !--RKF step 2
 !
@@ -151,25 +154,27 @@ subroutine step ()
     x(:,i) = xin(:,i) + a21*dt*velin(1:ndim,i)
     vel(:,i) = velin(:,i) + a21*dt*forcein(:,i)
 
-    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + dt*dBevoldtin(:,i)
-    if (icty.ge.1) rho(i) = rhoin(i) + dt*drhodtin(i)
-    if (ihvar.eq.1) then
-       !           hh(i) = hfact*(pmass(i)/rho(i))**dndim        ! my version
-       hh(i) = hhin(i)*(rhoin(i)/rho(i))**dndim                ! joe's           
-    elseif (ihvar.eq.2 .or. ihvar.eq.3) then
-       hh(i) = hhin(i) + dt*a21*dh1(i)
+    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + a21*dt*dBevoldtin(:,i)
+    if (icty.ge.1) rho(i) = rhoin(i) + a21*dt*drhodtin(i)
+    if (ihvar.eq.2 .or. ihvar.eq.3) then
+       hh(i) = hhin(i) + a21*dt*dh1(i)
     endif
-    if (iener.ne.0) en(i) = enin(i) + dt*dendtin(i)
-    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
-    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
+    if (iener.ne.0) en(i) = enin(i) + a21*dt*dendtin(i)
+!    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + a21*dt*daldtin(:,i),1.0)
+!    if (idivBzero.ge.2) psi(i) = psiin(i) + a21*dt*dpsidtin(i)
  enddo
 !
 !--calculate all derivatives
 !
  call derivs
- v2(:,:) = vel(:,:)
- f2(:,:) = force(:,:)
- dh2(:) = dhdtin(:)
+ do i=1,npart
+    v2(:,i)  = vel(:,i)
+    f2(:,i)  = force(:,i)
+    dB2(:,i) = dBevoldt(:,i)
+    dh2(i)   = dhdt(i)
+    den2(i)  = dendt(i)
+    drho2(i) = drhodt(i)
+ enddo
 !
 !--RK step 3
 !
@@ -177,25 +182,27 @@ subroutine step ()
     x(:,i) = xin(:,i) + (a31*v1(1:ndim,i) + a32*v2(1:ndim,i))*dt
     vel(:,i) = velin(:,i) + (a31*f1(:,i) + a32*f2(:,i))*dt
 
-    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + dt*dBevoldtin(:,i)
-    if (icty.ge.1) rho(i) = rhoin(i) + dt*drhodtin(i)
-    if (ihvar.eq.1) then
-       !           hh(i) = hfact*(pmass(i)/rho(i))**dndim        ! my version
-       hh(i) = hhin(i)*(rhoin(i)/rho(i))**dndim                ! joe's           
-    elseif (ihvar.eq.2 .or. ihvar.eq.3) then
-       hh(i) = hhin(i) + dt*(a31*dh1(i) + a32*dh2(i))
+    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + (a31*dB1(:,i) + a32*dB2(:,i))*dt
+    if (icty.ge.1) rho(i) = rhoin(i) + (a31*drho1(i) + a32*drho2(i))*dt
+    if (ihvar.eq.2 .or. ihvar.eq.3) then
+       hh(i) = hhin(i) + (a31*dh1(i) + a32*dh2(i))*dt
     endif
-    if (iener.ne.0) en(i) = enin(i) + dt*dendtin(i)
-    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
-    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
+    if (iener.ne.0) en(i) = enin(i) + (a31*den1(i) + a32*den2(i))*dt
+!    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
+!    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
  enddo
 !
 !--calculate all derivatives
 !
  call derivs
- v3(:,:) = vel(:,:)
- f3(:,:) = force(:,:)
- dh3(:) = dhdtin(:)
+ do i=1,npart
+    v3(:,i)  = vel(:,i)
+    f3(:,i)  = force(:,i)
+    dB3(:,i) = dBevoldt(:,i)
+    dh3(i)   = dhdt(i)
+    den3(i)  = dendt(i)
+    drho3(i) = drhodt(i)
+ enddo
 !
 !--RK step 4
 !
@@ -203,25 +210,27 @@ subroutine step ()
     x(:,i) = xin(:,i) + (a41*v1(1:ndim,i) + a42*v2(1:ndim,i) + a43*v3(1:ndim,i))*dt
     vel(:,i) = velin(:,i) + (a41*f1(:,i) + a42*f2(:,i) + a43*f3(:,i))*dt
 
-    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + dt*dBevoldtin(:,i)
-    if (icty.ge.1) rho(i) = rhoin(i) + dt*drhodtin(i)
-    if (ihvar.eq.1) then
-       !           hh(i) = hfact*(pmass(i)/rho(i))**dndim        ! my version
-       hh(i) = hhin(i)*(rhoin(i)/rho(i))**dndim                ! joe's           
-    elseif (ihvar.eq.2 .or. ihvar.eq.3) then
+    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + (a41*dB1(:,i) + a42*dB2(:,i) + a43*dB3(:,i))*dt
+    if (icty.ge.1) rho(i) = rhoin(i) + (a41*drho1(i) + a42*drho2(i) + a43*drho3(i))*dt
+    if (ihvar.eq.2 .or. ihvar.eq.3) then
        hh(i) = hhin(i) + dt*(a41*dh1(i) + a42*dh2(i) + a43*dh3(i))
     endif
-    if (iener.ne.0) en(i) = enin(i) + dt*dendtin(i)
-    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
-    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
+    if (iener.ne.0) en(i) = enin(i) + (a41*den1(i) + a42*den2(i) + a43*den3(i))*dt
+!    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
+!    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
  enddo
 !
 !--calculate all derivatives
 !
  call derivs
- v4(:,:) = vel(:,:)
- f4(:,:) = force(:,:)
- dh4(:) = dhdtin(:)
+ do i=1,npart
+    v4(:,i)  = vel(:,i)
+    f4(:,i)  = force(:,i)
+    dB4(:,i) = dBevoldt(:,i)
+    dh4(i)   = dhdt(i)
+    den4(i)  = dendt(i)
+    drho4(i) = drhodt(i)
+ enddo
 !
 !--RK step 5
 !
@@ -229,25 +238,27 @@ subroutine step ()
     x(:,i) = xin(:,i) + (a51*v1(1:ndim,i) + a52*v2(1:ndim,i) + a53*v3(1:ndim,i) + a54*v4(1:ndim,i))*dt
     vel(:,i) = velin(:,i) + (a51*f1(:,i) + a52*f2(:,i) + a53*f3(:,i) + a54*f4(:,i))*dt
 
-    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + dt*dBevoldtin(:,i)
-    if (icty.ge.1) rho(i) = rhoin(i) + dt*drhodtin(i)
-    if (ihvar.eq.1) then
-       !           hh(i) = hfact*(pmass(i)/rho(i))**dndim        ! my version
-       hh(i) = hhin(i)*(rhoin(i)/rho(i))**dndim                ! joe's           
-    elseif (ihvar.eq.2 .or. ihvar.eq.3) then
-       hh(i) = hhin(i) + dt*(a51*dh1(i) + a52*dh2(i) + a53*dh3(i) + a54*dh4(i))
+    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + (a51*dB1(:,i) + a52*dB2(:,i) + a53*dB3(:,i) + a54*dB4(:,i))*dt
+    if (icty.ge.1) rho(i) = rhoin(i) + (a51*drho1(i) + a52*drho2(i) + a53*drho3(i) + a54*drho4(i))*dt
+    if (ihvar.eq.2 .or. ihvar.eq.3) then
+       hh(i) = hhin(i) + (a51*dh1(i) + a52*dh2(i) + a53*dh3(i) + a54*dh4(i))*dt
     endif
-    if (iener.ne.0) en(i) = enin(i) + dt*dendtin(i)
-    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
-    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
+    if (iener.ne.0) en(i) = enin(i) + (a51*den1(i) + a52*den2(i) + a53*den3(i) + a54*den4(i))*dt
+!    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
+!    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
  enddo
 !
 !--calculate all derivatives
 !
  call derivs
- v5(:,:) = vel(:,:)
- f5(:,:) = force(:,:)
- dh5(:) = dhdtin(:)
+ do i=1,npart
+    v5(:,i)  = vel(:,i)
+    f5(:,i)  = force(:,i)
+    dB5(:,i) = dBevoldt(:,i)
+    dh5(i)   = dhdt(i)
+    den5(i)  = dendt(i)
+    drho5(i) = drhodt(i)
+ enddo
 !
 !--RK step 6
 !
@@ -256,63 +267,73 @@ subroutine step ()
          + a64*v4(1:ndim,i) + a65*v5(1:ndim,i))*dt
     vel(:,i) = velin(:,i) + (a61*f1(:,i) + a62*f2(:,i) + a63*f3(:,i) + &
          a64*f4(:,i) + a65*f5(:,i))*dt
-
-    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + dt*dBevoldtin(:,i)
-    if (icty.ge.1) rho(i) = rhoin(i) + dt*drhodtin(i)
-    if (ihvar.eq.1) then
-       !           hh(i) = hfact*(pmass(i)/rho(i))**dndim        ! my version
-       hh(i) = hhin(i)*(rhoin(i)/rho(i))**dndim                ! joe's           
-    elseif (ihvar.eq.2 .or. ihvar.eq.3) then
-       hh(i) = hhin(i) + dt*(a61*dh1(i) + a62*dh2(i) + a63*dh3(i) + a64*dh4(i) &
-            + a65*dh5(i))
+    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + dt*(a61*dB1(:,i) + a62*dB2(:,i) + a63*dB3(:,i) + &
+         a64*dB4(:,i) + a65*dB5(:,i))*dt
+    if (icty.ge.1) rho(i) = rhoin(i) + (a61*drho1(i) + a62*drho2(i) + a63*drho3(i) + a64*drho4(i) + a65*drho5(i))*dt
+    if (ihvar.eq.2 .or. ihvar.eq.3) then
+       hh(i) = hhin(i) + (a61*dh1(i) + a62*dh2(i) + a63*dh3(i) + a64*dh4(i) + a65*dh5(i))*dt
     endif
-    if (iener.ne.0) en(i) = enin(i) + dt*dendtin(i)
-    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
-    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
+    if (iener.ne.0) en(i) = enin(i) + (a61*den1(i) + a62*den2(i) + a63*den3(i) + a64*den4(i) + a65*den5(i))*dt
+!    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + dt*daldtin(:,i),1.0)
+!    if (idivBzero.ge.2) psi(i) = psiin(i) + dt*dpsidtin(i) 
  enddo
 !
 !--calculate all derivatives
 !
  call derivs
- v6(:,:) = vel(:,:)
- f6(:,:) = force(:,:)
- dh6(:) = dhdtin(:)
+ do i=1,npart
+    v6(:,i)  = vel(:,i)
+    f6(:,i)  = force(:,i)
+    dB6(:,i) = dBevoldt(:,i)
+    dh6(i)   = dhdt(i)
+    den6(i)  = dendt(i)
+    drho6(i) = drhodt(i)
+ enddo
 !
 !--RK final values
 !
  rkferr = 0.
+ rkferrratio = 1.
  do i=1,npart
 !--5th order result
     xtemp(:) = xin(:,i) + dt*(b15*v1(:,i) + b25*v2(:,i) + b35*v3(:,i) + &
          b45*v4(:,i) + b55*v5(:,i) + b65*v6(:,i))
     vtemp(:) = velin(:,i) + dt*(b15*f1(:,i) + b25*f2(:,i) + b35*f3(:,i) + &
          b45*f4(:,i) + b55*f5(:,i) + b65*f6(:,i))
+    if (imhd.ne.0 .and. iresist.ne.2) Btemp(:) = Bevolin(:,i) + dt*(b15*dB1(:,i) + b25*dB2(:,i) + b35*dB3(:,i) + &
+         b45*dB4(:,i) + b55*dB5(:,i) + b65*dB6(:,i))
 !--4th order result
     x(:,i) = xin(:,i) + dt*(b14*v1(:,i) + b24*v2(:,i) + b34*v3(:,i) + &
          b44*v4(:,i) + b54*v5(:,i))
     vel(:,i) = velin(:,i) + dt*(b14*f1(:,i) + b24*f2(:,i) + b34*f3(:,i) + &
          b44*f4(:,i) + b54*f5(:,i))
-
+    if (imhd.ne.0 .and. iresist.ne.2) Bevol(:,i) = Bevolin(:,i) + dt*(b14*dB1(:,i) + b24*dB2(:,i) + b34*dB3(:,i) + &
+         b44*dB4(:,i) + b54*dB5(:,i))
+    if (icty.ge.1) rho(i) = rhoin(i) + dt*(b14*drho1(i) + b24*drho2(i) + b34*drho3(i) + &
+         b44*drho4(i) + b54*drho5(i))
+    if (iener.ne.0) en(i) = enin(i) + dt*(b14*den1(i) + b24*den2(i) + b34*den3(i) + &
+         b44*den4(i) + b54*den5(i))
 !    print *, 'Vs ', vel(3,i), vtemp(3)
 !    rkferr = abs(vtemp(3) - vel(3,i))
 !    rkferr = abs(vtemp(3)/vel(3,i) - 1.)
     do ii = 1, ndim
 !       rkferr = MAX(rkferr, abs(vtemp(ii) - vel(ii,i)), abs(xtemp(ii) - x(ii,i)))
        rkferr = MAX(rkferr, abs(vtemp(ii)/vel(ii,i) - 1.), abs(xtemp(ii)/x(ii,i)) - 1.)
+       if (imhd.ne.0 .and. iresist.ne.2) then
+          errmhd = abs(Btemp(ii)/Bevol(ii,i)) - 1.
+          !if (errmhd > rkferr) then 
+            ! rkferr = errmhd
+            ! print*, 'setting rkferr = ',rkferr
+          !endif
+       endif
     enddo
 !    print *, 'rkerr ', rkferr
     rkferrratio = rkferr/rkftol
 
-    if (icty.ge.1) rho(i) = rhoin(i) + hdt*(drhodt(i)+drhodtin(i))
-    if (ihvar.eq.2) then
-       hh(i) = hhin(i) + hdt*(dhdt(i)+dhdtin(i))
-       if (hh(i).le.0.) then
-          write(iprint,*) 'step: hh -ve ',i,hh(i)
-          call quit
-       endif
+    if (ihvar.eq.2 .or. ihvar.eq.3) then
+       hh(i) = hhin(i) + dt*(b14*dh1(i) + b24*dh2(i) + b34*dh3(i) + b44*dh4(i) + b54*dh5(i))
     endif
-    if (iener.ne.0) en(i) = enin(i) + hdt*(dendt(i)+dendtin(i))
-    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + hdt*(daldt(:,i)+daldtin(:,i)),1.0)
+!    if (any(iavlim.ne.0)) alpha(:,i) = min(alphain(:,i) + hdt*(daldt(:,i)+daldtin(:,i)),1.0)
   enddo
 
   if (rkferr.lt.tiny(0.)) then
