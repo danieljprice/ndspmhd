54d713f90e186dc41c044286bc4f165fa583f087
Daniel Price <daniel.price@monash.edu>
2015-02-04 11:58:03 +1100

    improved discvert setup; spits out lots of info; converts to/from physical units; adds dust via modify_dump routine

--- src/setup_discvert2D.f90
+++ src/setup_discvert2D.f90
@@ -46,6 +46,8 @@ subroutine setup
  integer :: i
  real :: massp,rhomin
  real :: partvol,denszero,HonR,H0,omega,cs0,totmass
+ real :: umass,utime,udist,solarm,au,gg,years,sigma
+ real :: t_cour,t_stop,graindens,grainsize,mm,micron
  logical :: equalmass
 !
 !--allow for tracing flow
@@ -69,25 +71,58 @@ subroutine setup
  xmax(2) = 3.*H0
  equalmass = .true.
 
+! constants to translate to physical units
+ mm = 0.1       ! 1 mm in cm
+ micron = 1.e-4 ! 1 micron in cm
+ au = 1.496d13
+ solarm = 1.9891d33
+ gg = 6.672d-8
+ years = 3.1556926d7
+ umass = solarm
+ udist = 10.*au
+ utime = sqrt(udist**3/(gg*umass))
 !
 !--determine particle mass
 !
- denszero = 1.e-6   ! midplane density
+ denszero = 1.e-3   ! midplane density
  rhomin   = 1.e-12  ! minimum density
  partvol = psep**ndim
  massp = partvol*denszero ! average particle mass
  omega = sqrt(Mstar/Rdisc**3)
  !H0 = 0.3921651256
+ print*,' rho0  = ',denszero,' in g/cm^3 = ',denszero*(umass/udist**3)
  print*,' Rdisc = ',Rdisc,' Mstar = ',Mstar
+ print*,' Omega = ',omega,' t_orb = ',2.*pi/omega,' in years = ',2.*pi/omega*utime/years
  
  cs0 = H0*omega !0.0453 !H0*omega
  polyk = cs0**2
- print*,' cs0 = ',cs0
+ print*,' cs0   = ',cs0
  H0 = cs0/omega
  !cs_physical = cs0*
  !print*,' temperature = ',1.38e-13*,' assuming distance in AU, mass in Msun and G=1'
  print*,' polyk = ',polyk
- print*,' scale height H0 = ',H0
+ print*,' scaleheight H =',H0
+!
+!--set particle mass from actual integral of surface density profile
+!
+ totmass = 2.*denszero*sqrt(0.5*pi)*H0*erf(xmax(2)/(sqrt(2.)*H0))*(xmax(1) - xmin(1))
+ print*,' totmass       =',totmass, ' in MSun =',totmass*umass/solarm
+ 
+ ! various information about grain sizes and the stopping time
+ sigma = 2.*denszero*sqrt(0.5*pi)*H0
+ print*,' Sigma         =',sigma,' in g/cm^2 =',sigma*(umass/udist**2)
+ t_cour = 1.2*psep/cs0
+ print*,' t_courant     =',t_cour,' t_cour/t_orb =',t_cour/(2.*pi/omega)
+ graindens = 3./(umass/udist**3)
+ grainsize = 1.*mm/udist
+ print*,' grain size    =',grainsize,' in cm     =',grainsize*udist,' in m =',grainsize*udist*0.01
+ print*,' grain density =',graindens,' in g/cm^3 =',graindens*(umass/udist**3)
+
+ t_stop = graindens*grainsize/(denszero*cs0)*sqrt(pi/8.)
+ print*,' ts (z=0)      =',t_stop,' ts*omega  =',t_stop*omega,' ts/t_cour =',t_stop/t_cour
+ t_stop = graindens*grainsize/(denszero*exp(-0.5*(2.)**2)*cs0)*sqrt(pi/8.)
+ print*,' ts (z=2H)     =',t_stop,' ts*omega  =',t_stop*omega,' ts/t_cour =',t_stop/t_cour
+ print*,' ts*omega (est)=',graindens*grainsize/sigma
 
  if (equalmass) then
     call set_uniform_cartesian(2,psep,xmin,xmax,adjustbound=.true.,stretchdim=2,stretchfunc=rhoy) 
@@ -95,12 +130,8 @@ subroutine setup
     call set_uniform_cartesian(2,psep,xmin,xmax,adjustbound=.true.)
  endif
  npart = ntotal
- print*,'npart =',npart
-!
-!--set particle mass from actual integral of surface density profile
-!
- totmass = 2.*denszero*sqrt(0.5*pi)*H0*erf(xmax(2)/(sqrt(2.)*H0))
- print*,' totmass = ',totmass, ' massp = ',totmass/npart,massp
+ print*,' npart =',npart
+ print*,' massp = ',totmass/npart,massp
  massp = totmass/npart
 !
 !--now assign particle properties
@@ -138,3 +169,42 @@ contains
  end function rhoy
 
 end subroutine setup
+
+subroutine modify_dump
+ use loguns,       only:iprint
+ use part
+ use options,        only:idust
+ use timestep,       only:time
+ use mem_allocation, only:alloc
+ implicit none
+ integer :: i,j
+ real :: dust_to_gas_ratio
+ 
+ time = 0.
+ dust_to_gas_ratio = 1.e-2
+ 
+ if (idust==2) then
+    write(iprint,"(/,a,/)") ' > MODIFYING dump by adding dust (two-fluid) <'
+    call alloc(2*ntotal)
+    do i=1,npart
+       itype(i) = itypegas
+       j = npart+i
+       call copy_particle(j,i)
+       x(:,j) = x(:,i)
+       vel(:,j) = vel(:,i)
+       pmass(j) = pmass(i)*dust_to_gas_ratio
+       dens(j)  = dens(i)*dust_to_gas_ratio
+       uu(j)    = 0.
+       itype(j) = itypedust
+    enddo
+    npart = 2*npart
+ elseif (idust==1 .or. idust==3 .or. idust==4) then
+    write(iprint,"(/,a,/)") ' > MODIFYING dump by adding dust (one-fluid) <'
+    do i=1,npart
+       dustfrac(i) = dust_to_gas_ratio/(1. + dust_to_gas_ratio)
+       pmass(i)    = pmass(i)*(1. + dust_to_gas_ratio)
+       deltav(:,i) = 0.
+    enddo
+ endif
+
+end subroutine modify_dump
